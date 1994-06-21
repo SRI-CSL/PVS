4522de2ef3111d41317513a37d45573a64447264
cwitty <cwitty@ab98feae-9515-0410-836f-c1e8bd48b6aa>
1994-06-21 20:47:55 +0000

    Working version after rewrite.
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@5 ab98feae-9515-0410-836f-c1e8bd48b6aa

--- wish/pvs-support.tcl
+++ wish/pvs-support.tcl
@@ -8,167 +8,212 @@ proc emacs-eval {arg} {
 set proof_x_sep 10
 set proof_y_sep 20
 
-proc insert-proofstate {prf path win} {
-    global env
-    $win create bitmap 0 0 -bitmap @$env(PVSPATH)/wish/sequent.xbm \
-	-tags "$path.sequent sequent" -anchor n
-    $win create line 0 0 0 0 -tags "$path.totop totop $path.sequpdate"
-    $win create line 0 0 0 0 \
-	-tags "$path.torule torule $path.sequpdate $path.ruleupdate"
-    $win create text 0 0 -text [lindex $prf 1] \
-	-tags "$path.rule rule" -anchor n
+proc create-dag {win} {
+    upvar #0 dag-$win dag
+
+    catch {unset dag}
 }
 
-proc insert-proof {prf path win} {
-    insert-proofstate $prf $path $win
+proc dag-add-item {win tag succs} {
+    upvar #0 dag-$win dag
 
-    set count 1
-    foreach sub [lindex $prf 2] {
-	insert-proof $sub $path.$count $win
-	$win addtag $path.ruleupdate withtag $path.$count.totop
-	incr count
+    set dag(succs,$tag) $succs
+
+    foreach item [$win find withtag $tag] {
+	set dag(idtotag,$item) $tag
+	if {![string match *dagline* [$win gettags $item]]} {
+	    $win addtag $tag.real withtag $item
+	}
+    }
+
+    set bbox [$win bbox $tag.real]
+    set dag(anchor,$tag) [list [expr -[lindex $bbox 0]] \
+			      [expr -[lindex $bbox 1]]]
+
+    # Give these initial values (it doesn't matter what)
+    set dag(topx,$tag) 0
+    set dag(botx,$tag) 0
+    set dag(topy,$tag) 0
+    set dag(boty,$tag) 0
+
+    foreach s $succs {
+	set lineid \
+	    [$win create line 0 0 0 0 \
+		 -tags "$s dagline$tag,$s dagline.from$tag dagline.to$s dagline"]
+	set dag(linefrom,$lineid) $tag
+	set dag(lineto,$lineid) $s
     }
 }
 
-proc dag-add-item {tag succs} {
+proc dag-delete-item {tag} {
+    error "You can't delete items yet"
 }
 
-proc dag-delete-item {tag} {
+proc move-dag-item {win tag x y} {
+    upvar #0 dag-$win dag
+
+    set bbox [$win bbox $tag.real]
+    set anchor $dag(anchor,$tag)
+
+    set curx [expr [lindex $bbox 0]+[lindex $anchor 0]]
+    set cury [expr [lindex $bbox 1]+[lindex $anchor 1]]
+
+    $win move $tag [expr $x-$curx] [expr $y-$cury]
+
+    set dag(topx,$tag) $x
+    set dag(topy,$tag) $y
+
+    set dag(botx,$tag) $x
+    set dag(boty,$tag) [expr $y+[lindex $bbox 3]-[lindex $bbox 1]]
+
+    update-lines-to $win $tag
+    update-lines-from $win $tag
+}
+
+proc update-lines-to {win tag} {
+    foreach id [$win find withtag dagline.to$tag] {
+	update-line $win $id
+    }
+}
+
+proc update-lines-from {win tag} {
+    foreach id [$win find withtag dagline.from$tag] {
+	update-line $win $id
+    }
+}
+
+proc update-line {win id} {
+    upvar #0 dag-$win dag
+
+    $win coords $id \
+	$dag(botx,$dag(linefrom,$id)) \
+	$dag(boty,$dag(linefrom,$id)) \
+	$dag(topx,$dag(lineto,$id)) \
+	$dag(topy,$dag(lineto,$id))
 }
 
 proc dag-layout {top} {
 }
 
-proc tree-layout {top} {
+proc tree-layout {win top} {
+    update-widths $win $top
+    move-tree $win $top
 }
 
-proc proof-rule {path rule} {
-    global $path
+proc update-widths {win tag} {
+    upvar #0 dag-$win dag
 
-    set $path(rule) $rule
+    global proof_x_sep
+
+    set bbox [$win bbox $tag.real]
+    set width [expr [lindex $bbox 2]-[lindex $bbox 0]]
+
+    set kw -$proof_x_sep
+    foreach sub $dag(succs,$tag) {
+	update-widths $win $sub
+	incr kw $dag(width,$sub)
+	incr kw $proof_x_sep
+    }
+
+    set dag(width,$tag) [expr {$kw>$width?$kw:$width}]
+}
+
+proc move-tree {win tag {x 0} {y 0}} {
+    upvar #0 dag-$win dag
+    global proof_x_sep
+    global proof_y_sep
+
+    move-dag-item $win $tag $x $y
+
+    set bbox [$win bbox $tag.real]
+
+    set kw -$proof_x_sep
+    foreach succ $dag(succs,$tag) {
+	incr kw $dag(width,$succ)
+	incr kw $proof_x_sep
+    }
+
+    set curx [expr round($x-$kw/2)]
+    foreach succ $dag(succs,$tag) {
+	move-tree $win $succ \
+	    [expr $curx+$dag(width,$succ)/2] \
+	    [expr $proof_y_sep+[lindex $bbox 3]]
+	incr curx $dag(width,$succ)
+	incr curx $proof_x_sep
+    }
+}
+
+proc ancestors {path {suffix {}}} {
+    if {$path=={top}} {
+	return {}
+    } else {
+	regexp {^(.*)\.([^.]+)$} $path whole pre post
+	return [concat $pre$suffix [ancestors $pre $suffix]]
+    }
+}
+
+proc kids {path nkids} {
+    set kids {}
+    for {set i 0} {$i<$nkids} {incr i} {
+	lappend kids $path.$i
+    }
+    return $kids
 }
 
 proc proof-num-children {path kids} {
     global $path
 
-    set $path(kids) $kids
+    set ${path}(kids) $kids
+    catch {unset ${path}(rule)}
+    catch {unset ${path}(done)}
+}
+
+proc proof-rule {path rule} {
+    global $path
+
+    set ${path}(rule) $rule
 }
 
 proc proof-done {path} {
     global $path
 
-    set $path(done) 1
+    set ${path}(done) 1
 }
 
 proc proof-show {path} {
-    global $path proofwin env
+    global $path proofwin env proof_y_sep
 
-    if [info exists $path(sequent)] {
+    if [info exists ${path}(sequent)] {
 	show-sequent $path
     } else {
-	$proofwin create bitmap 0 0 -bitmap @$env(PVSPATH)/wish/sequent.xbm \
-	    -tags "$path.sequent $path sequent [ancestors $path]" -anchor n
-	if [info exists $path(rule)] {
-	    $proofwin create line 0 0 0 0 \
-		-tags "$path.line $path [ancestors $path]"
-	    $proofwin create text 0 0 -text [set $path(rule)]
+	set seq \
+	    [$proofwin create bitmap 0 0 \
+		 -bitmap @$env(PVSPATH)/wish/sequent.xbm \
+		 -tags "$path.sequent $path sequent [ancestors $path .desc]" \
+		 -anchor n]
+	if [info exists ${path}(rule)] {
+	    set bbox [$proofwin bbox $seq]
+	    set seqbot [lindex $bbox 3]
+	    set linebot [expr $seqbot+$proof_y_sep]
+	    $proofwin create line 0 $seqbot 0 $linebot \
+		-tags "$path.line $path [ancestors $path .desc]"
+	    $proofwin create text 0 $linebot -text [set ${path}(rule)] \
+		-tags "$path.rule $path rule [ancestors $path .desc]" \
+		-anchor n
 	}
     }
 
-    dag-add-item $path [kids $path [set $path(kids)]]
+    dag-add-item $proofwin $path [kids $path [set ${path}(kids)]]
 }
 
-proc update-widths {prf path win} {
-    global path_width proof_x_sep
+proc layout-proof {} {
+    global proofwin
 
-    set bbox [$win bbox $path.rule]
-    set width [expr {[lindex $bbox 2] - [lindex $bbox 0]}]
-
-    set kw -$proof_x_sep
-    set count 1
-    foreach sub [lindex $prf 2] {
-	update-widths $sub $path.$count $win
-	incr kw $path_width($path.$count)
-	incr kw $proof_x_sep
-	incr count
-    }
-
-    set path_width($path) [expr {$kw>$width?$kw:$width}]
+    tree-layout $proofwin top
+    center-canvas $proofwin
 }
 
-proc move-sequent {win path x y} {
-    $win coords $path.sequent $x $y
-    update-lines $win $path.sequpdate
-}
-
-proc move-rule {win path x y} {
-    $win coords $path.rule $x $y
-    update-lines $win $path.ruleupdate
-}
-
-proc update-lines {win tag} {
-    foreach line [$win find withtag $tag] {
-	foreach tag [$win gettags $line] {
-	    if [regexp {^(.*)\.([0-9]+)\.totop} $tag whole parpath kid] {
-		set rulebbox [$win bbox $parpath.rule]
-		set rulexy [$win coords $parpath.rule]
-		set seqxy [$win coords $parpath.$kid.sequent]
-		eval $win coords $line \
-		    [lindex $rulexy 0] [lindex $rulebbox 3] \
-		    $seqxy
-		break
-	    } elseif [regexp {^(.*)\.torule} $tag whole curpath] {
-		set rulexy [$win coords $curpath.rule]
-		set seqbbox [$win bbox $curpath.sequent]
-		set seqxy [$win coords $curpath.sequent]
-		eval $win coords $line \
-		    [lindex $seqxy 0] [lindex $seqbbox 3] \
-		    $rulexy
-		break
-	    }
-	}
-    }
-}
-
-proc move-proof {prf path win x y} {
-    global path_width proof_x_sep proof_y_sep
-
-    set numkids [llength [lindex $prf 2]]
-
-    move-sequent $win $path $x $y
-    set seqbbox [$win bbox $path.sequent]
-
-    move-rule $win $path \
-	$x [expr $proof_y_sep+[lindex $seqbbox 3]]
-    set rulebbox [$win bbox $path.rule]
-
-    set kw -$proof_x_sep
-
-    for {set i 1} {$i<=$numkids} {incr i} {
-	incr kw $path_width($path.$i)
-	incr kw $proof_x_sep
-    }
-
-    set curx [expr round($x-$kw/2)]
-
-    set count 1
-    foreach sub [lindex $prf 2] {
-	move-proof $sub $path.$count $win \
-	    [expr $curx+$path_width($path.$count)/2] \
-	    [expr $proof_y_sep+[lindex $rulebbox 3]]
-	incr curx $path_width($path.$count)
-	incr curx $proof_x_sep
-	incr count
-    }
-}	
-
-proc update-proof {prf path win} {
-    global path_width
-
-    update-widths $prf $path $win
-
-    move-proof $prf $path $win 0 15
-
+proc center-canvas {win} {
     set allbbox [$win bbox all]
     set allbbox [lreplace $allbbox 1 1 [expr [lindex $allbbox 1]-10]]
     $win config -scrollregion $allbbox
@@ -236,11 +281,35 @@ proc gen-ps {top} {
     show-message $top "Saved PS to $psfile"
 }
 
-# For debugging.
-proc reshow-proof {} {
-    global proof
-    show-proof $proof(proof) $proof(name) $proof(file) $proof(directory)
-}
+proc setup-proof {name file directory} {
+    global proof proofwin
+    set proof(name) $name
+    set proof(file) $file
+    set proof(directory) $directory
+    catch {destroy .show-proof}
+    set top [toplevel .show-proof -geometry 400x400]
+    pack propagate $top 0
+    wm title $top "Proof of $name in $directory$file"
+    wm iconname $top "PVS Proof"
+    wm minsize $top 100 100
+    wm maxsize $top 10000 10000
+    set fr [frame $top.fr]
+    pack $fr -expand yes -fill both
+    set c [canvas $fr.c -xscroll "$fr.hscroll set" -yscroll "$fr.vscroll set"]
+    set proofwin $c
+    create-dag $c
+    scrollbar $fr.vscroll -relief sunken -command "$c yview"
+    scrollbar $fr.hscroll -relief sunken -command "$c xview" -orient horiz
+    pack $fr.vscroll -side right -fill y
+    pack $fr.hscroll -side bottom -fill x
+    pack $c -expand yes -fill both
+    label $top.message -text ""
+    pack $top.message -fill x
+    button $top.dismiss -text "Dismiss" -command {destroy .show-proof}
+    pack $top.dismiss -side left -padx 2 -pady 2
+    button $top.ps -text "Gen PS" -command "gen-ps $top"
+    pack $top.ps -side left -padx 2 -pady 2
+}    
 
 proc show-proof {prf name file directory} {
     global proof
