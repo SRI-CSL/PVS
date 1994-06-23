ab45489c58b33cb7a1e1da76000e69df2957ed0c
cwitty <cwitty@ab98feae-9515-0410-836f-c1e8bd48b6aa>
1994-06-23 01:29:20 +0000

    Working code with colors and automove.
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@7 ab98feae-9515-0410-836f-c1e8bd48b6aa

--- wish/pvs-support.tcl
+++ wish/pvs-support.tcl
@@ -5,6 +5,18 @@ proc emacs-eval {arg} {
     gets stdin
 }
 
+proc emacs-evaln {arg} {
+    puts stdout ":pvs-evaln $arg :end-pvs-evaln"
+}
+
+proc pvs-send {arg} {
+    emacs-evaln [format {(pvs-send '%s)} $arg]
+}
+
+proc pvs-send-and-wait {arg} {
+    emacs-eval [format {(pvs-send-and-wait '%s)} $arg]
+}
+
 set proof_x_sep 10
 set proof_y_sep 20
 
@@ -21,9 +33,6 @@ proc dag-add-item {win tag succs} {
 
     foreach item [$win find withtag $tag] {
 	set dag(idtotag,$item) $tag
-	if {![string match *dagline* [$win gettags $item]]} {
-	    $win addtag $tag.real withtag $item
-	}
     }
 
     set bbox [$win bbox $tag.real]
@@ -46,7 +55,24 @@ proc dag-add-item {win tag succs} {
 }
 
 proc dag-delete-item {win tag suffix} {
+    upvar #0 dag-$win dag
+
     $win delete $tag$suffix
+    if {$suffix=={}} {
+	if {[info exists dag(destroy,$tag)]} {
+	    eval $dag(destroy,$tag)
+	}
+    }
+}
+
+proc dag-add-destroy-cb {win tag cb} {
+    upvar #0 dag-$win dag
+
+    if {[info exists dag(destroy,$tag)]} {
+	set dag(destroy,$tag) [concat $cb ";" $dag(destroy,$tag)]
+    } else {
+	set dag(destroy,$tag) $cb
+    }
 }
 
 proc dag-delete-subtree {win tag {suffix .real}} {
@@ -176,6 +202,8 @@ proc kids {path nkids} {
 proc delete-proof-subtree {path} {
     global proofwin
 
+    catch {move-dag-item $proofwin $path 0 0}
+
     dag-delete-subtree $proofwin $path
 }
 
@@ -185,6 +213,7 @@ proc proof-num-children {path kids} {
     set ${path}(kids) $kids
     catch {unset ${path}(rule)}
     catch {unset ${path}(done)}
+    catch {unset ${path}(tcc)}
 }
 
 proc proof-rule {path rule} {
@@ -194,9 +223,17 @@ proc proof-rule {path rule} {
 }
 
 proc proof-done {path} {
-    global $path
+    global $path proofwin
 
     set ${path}(done) 1
+
+    my-foreground $proofwin $path blue
+}
+
+proc proof-tcc {path} {
+    global $path
+
+    set ${path}(tcc) 1
 }
 
 proc proof-show {path} {
@@ -208,29 +245,111 @@ proc proof-show {path} {
 	set seq \
 	    [$proofwin create bitmap 0 0 \
 		 -bitmap @$env(PVSPATH)/wish/sequent.xbm \
-		 -tags "$path.sequent $path sequent [ancestors $path .desc]" \
+		 -tags "$path.sequent $path $path.real sequent [ancestors $path .desc]" \
 		 -anchor n]
 	if [info exists ${path}(rule)] {
 	    set bbox [$proofwin bbox $seq]
 	    set seqbot [lindex $bbox 3]
 	    set linebot [expr $seqbot+$proof_y_sep]
 	    $proofwin create line 0 $seqbot 0 $linebot \
-		-tags "$path.line $path [ancestors $path .desc]"
+		-tags "$path.line $path $path.real [ancestors $path .desc]"
 	    $proofwin create text 0 $linebot -text [set ${path}(rule)] \
-		-tags "$path.rule $path rule [ancestors $path .desc]" \
+		-tags "$path.rule $path $path.real rule [ancestors $path .desc]" \
 		-anchor n
 	}
     }
 
+
     dag-add-item $proofwin $path [kids $path [set ${path}(kids)]]
     if [info exists ${path}(done)] {
 	my-foreground $proofwin $path blue
+    } elseif [info exists ${path}(tcc)] {
+	my-foreground $proofwin $path green4
     }
+}
+
+proc get-current-sequent {} {
+    global proofwin
+    upvar #0 dag-$proofwin dag
+
+    set path $dag(idtotag,[$proofwin find withtag current])
+
+    set lisp_path [path-to-lisp-path $path]
+
+    source [lindex [pvs-send-and-wait "(request-sequent '($lisp_path))"] 0]
+}
+
+proc show-sequent {path seq_label text} {
+    global proofwin pathtolabel
+
+    for {set label 1} {[winfo exists .sequent$label]} {incr label} {
+    }
+
+    set pathtolabel($path) $label
+
+    set win .sequent$label.sequent
+    set bbox [$proofwin bbox $path.sequent]
+    set x [lindex $bbox 2]
+    set y [expr round(([lindex $bbox 1]+[lindex $bbox 3])/2)]
+    $proofwin create text $x $y -anchor w \
+	-tags "$path $path.label $path.label$label label [ancestors $path .desc] label.$label" \
+	-text $label
+    update-color $path $path.label$label
+    
+    frame .sequent$label
+    toplevel $win -class Sequent
+    text $win.text -setgrid true
+    $win.text insert end $text
+    set height [lindex [split [$win.text index end] .] 0]
+    pack $win.text
+    button $win.dismiss -text Dismiss -command "destroy $win"
+    pack $win.dismiss -side left -padx 2 -pady 2
+    bind $win <Destroy> "catch {$proofwin delete $path.label$label}"
+    bind $win <Destroy> "+after 1 {catch {destroy .sequent$label}}"
+    wm geometry $win 80x$height
+    wm iconname $win {PVS sequent}
+    wm title $win "Sequent $label ($seq_label)"
+
+    dag-add-destroy-cb $proofwin $path "disable-sequent $win"
+}
+
+proc disable-sequent {win} {
+    catch {destroy $win}
+}
+
+proc path-to-lisp-path {path} {
+    lrange [split $path .] 1 end
 }
 
 proc my-foreground {win tag color} {
     my-itemconfig $win $tag -foreground $color
     my-itemconfig $win $tag -fill $color
+}
+
+proc update-color {path tag} {
+    global proofwin $path
+
+    if [info exists ${path}(done)] {
+	my-foreground $proofwin $tag blue
+	return
+    }
+
+    set seqid [$proofwin find withtag $path.sequent]
+    set tags [$proofwin gettags $seqid]
+    foreach t $tags {
+	if {$t=={current-subgoal}} {
+	    my-foreground $proofwin $tag firebrick
+	    $proofwin addtag current-subgoal withtag $tag
+	    return
+	}
+    }
+
+    if [info exists ${path}(tcc)] {
+	my-foreground $proofwin $tag green4
+	return
+    }
+    
+    my-foreground $proofwin $tag black
 }
 
 proc my-itemconfig {win tag opt val} {
@@ -249,24 +368,64 @@ proc layout-proof {} {
 proc center-canvas {win} {
     set allbbox [$win bbox all]
     set allbbox [lreplace $allbbox 1 1 [expr [lindex $allbbox 1]-10]]
+    set allbbox [lreplace $allbbox 3 3 [expr [lindex $allbbox 3]+10]]
     $win config -scrollregion $allbbox
     update idletasks
     set winwid [winfo width $win]
     set bboxwid [expr [lindex $allbbox 2]-[lindex $allbbox 0]]
     set margin [expr ($winwid-$bboxwid)/2]
-    $win xview [expr -$margin / [lindex [$win config -scrollincrement] 4]]
+#    $win xview [expr -$margin / [lindex [$win config -scrollincrement] 4]]
 }
 
 proc proof-current {path} {
-    global proofwin
+    global proofwin curpath
 
-    my-foreground $proofwin current black
-    $proofwin dtag current
-    $proofwin addtag current withtag $path
-    foreach tag [ancestors $path] {
-	$proofwin addtag current withtag $tag
+    if {[info exists curpath]} {
+	$proofwin dtag current-subgoal
+	foreach tag [concat $curpath [ancestors $curpath]] {
+	    update-color $tag $tag
+	}
     }
-    my-foreground $proofwin current firebrick
+    $proofwin addtag current-subgoal withtag $path
+    foreach tag [ancestors $path] {
+	$proofwin addtag current-subgoal withtag $tag
+    }
+    my-foreground $proofwin current-subgoal firebrick
+    my-foreground $proofwin $path orange
+
+    set bbox [$proofwin bbox $path.real]
+    regexp {^(.*).c} $proofwin whole fr
+    set hscroll $fr.hscroll
+    set vscroll $fr.vscroll
+
+    set hget [$hscroll get]
+    set vget [$vscroll get]
+
+    set units [lindex [$proofwin config -scrollincrement] 4]
+
+    set allbbox [lindex [$proofwin config -scrollregion] 4]
+
+    set width [winfo width $proofwin]
+    set height [winfo height $proofwin]
+
+    set left [expr $units*[lindex $hget 2]+[lindex $allbbox 0]]
+    set top [expr $units*[lindex $vget 2]+[lindex $allbbox 1]]
+    set right [expr $left+$width]
+    set bottom [expr $top+$height]
+
+    if {[lindex $bbox 3]+10>$bottom} {
+	$proofwin yview [expr ([lindex $bbox 3]+10-$height-[lindex $allbbox 1])/$units]
+    } elseif {[lindex $bbox 1]-10<$top} {
+	$proofwin yview [expr ([lindex $bbox 1]-10-[lindex $allbbox 1])/$units]
+    }
+
+    if {[lindex $bbox 2]+10>$right} {
+	$proofwin xview [expr ([lindex $bbox 2]+10-$width-[lindex $allbbox 0])/$units]
+    } elseif {[lindex $bbox 0]-10<$left} {
+	$proofwin xview [expr ([lindex $bbox 0]-10-[lindex $allbbox 0])/$units]
+    }				      
+
+    set curpath $path
 }
 
 set proof(lastX) 0
@@ -340,7 +499,8 @@ proc setup-proof {name file directory} {
     wm maxsize $top 10000 10000
     set fr [frame $top.fr]
     pack $fr -expand yes -fill both
-    set c [canvas $fr.c -xscroll "$fr.hscroll set" -yscroll "$fr.vscroll set"]
+    set c [canvas $fr.c -xscroll "$fr.hscroll set" -yscroll "$fr.vscroll set" \
+	      -height 1 -width 1]
     set proofwin $c
     create-dag $c
     scrollbar $fr.vscroll -relief sunken -command "$c yview"
@@ -348,12 +508,21 @@ proc setup-proof {name file directory} {
     pack $fr.vscroll -side right -fill y
     pack $fr.hscroll -side bottom -fill x
     pack $c -expand yes -fill both
+    $c bind sequent <1> {get-current-sequent}
     label $top.message -text ""
-    pack $top.message -fill x
+    pack $top.message -fill x -side bottom
     button $top.dismiss -text "Dismiss" -command {destroy .show-proof}
     pack $top.dismiss -side left -padx 2 -pady 2
     button $top.ps -text "Gen PS" -command "gen-ps $top"
     pack $top.ps -side left -padx 2 -pady 2
+    bind $top <Destroy> {pvs-send (stop-displaying-proof)}
+    bind $top <Destroy> {+
+	foreach kid [winfo children .] {
+	    if {[string match .sequent* $kid]} {
+		destroy $kid
+	    }
+	}
+    }
 }    
 
 proc show-proof {prf name file directory} {
