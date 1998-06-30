e7b9a80a9924b52edf2aea7a956cb5f59d3ff128
cyrluk <cyrluk@ab98feae-9515-0410-836f-c1e8bd48b6aa>
1998-06-30 01:32:13 +0000

    Initial revision
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@70 ab98feae-9515-0410-836f-c1e8bd48b6aa

---
+++ src/decision-procedures/arrays.lisp
@@ -0,0 +1,48 @@
+(in-package dp)
+
+(defun update-p (term)
+  (and (application-p term)
+       (eq (funsym term) *update*)))
+
+(defun applyupdate-p (term)
+  (and (application-p term)
+       (update-p (funsym term))))
+
+(defmacro mk-update (array index val)
+  `(mk-term (list *update* ,array ,index ,val)))
+
+(defmacro update-array (term)
+  `(arg 1 ,term))
+
+(defmacro update-index (term)
+  `(arg 2 ,term))
+
+(defmacro update-value (term)
+  `(arg 3 ,term))
+
+(defmacro mk-apply (array index)
+  `(mk-term (list ,array ,index)))
+
+(defmacro applyupdate-array (term)
+  `(funsym ,term))
+
+(defmacro applyupdate-index (term)
+  `(arg 1 ,term))
+
+(defun test-equality (term1 term2 *dp-state*)
+  (let* ((equality (mk-equality term1 term2))
+	 (norm-equality (sigma equality *dp-state*)))
+    norm-equality))
+
+(defun sigapplyupdate (term cong-state)
+  (let* ((update (applyupdate-array term))
+	 (apply-index (applyupdate-index term))
+	 (array1 (update-array update))
+	 (update-index (update-index update))
+	 (val  (update-value update)))
+    (let ((update-index_eq_apply-index
+	   (test-equality  update-index apply-index cong-state)))
+      (cond ((true-p update-index_eq_apply-index) val)
+	    ((false-p update-index_eq_apply-index)
+	     (sigma (mk-term (list array1 apply-index)) cong-state))
+	    (t term)))))
