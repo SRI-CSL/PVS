bce6392a737404f9cc7be5c7bd8b64ae0e11ffee
owre <owre@ab98feae-9515-0410-836f-c1e8bd48b6aa>
1998-10-31 10:38:18 +0000

    Initial revision
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@130 ab98feae-9515-0410-836f-c1e8bd48b6aa

---
+++ src/prover/expand.lisp
@@ -0,0 +1,320 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; -*- Mode: Lisp -*- ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; expand.lisp -- 
+;; Author          : Sam Owre
+;; Created On      : Sat Oct 31 02:31:26 1998
+;; Last Modified By: Sam Owre
+;; Last Modified On: Sat Oct 31 02:35:40 1998
+;; Update Count    : 1
+;; Status          : Unknown, Use with caution!
+;; 
+;; HISTORY
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(in-package 'PVS)
+
+
+
+(defvar *count-occurrences* 0)
+(defvar *if-simplifies*)
+;(defvar *dependent-decls*)
+
+
+(defun expand (name &optional sformnum occurrence if-simplifies
+		    assert?)
+  #'(lambda (ps)
+      (expand-step name ps sformnum occurrence if-simplifies
+		   assert?)))
+
+(defun expand-step (name ps sformnum occurrence if-simplifies assert?)
+  (let* ((goalsequent (current-goal ps))
+	 (alists (alists ps))
+	 (sigalist (dpinfo-sigalist  alists))
+	 (findalist (dpinfo-findalist  alists))
+	 (usealist (dpinfo-usealist alists))
+	 ;;(typealist (typealist alists)) ;;NSH(2.16.94)
+	 (*assert-flag* (if (eq assert? 'none) 'none
+			    (if assert? 'assert 'simplify)))
+	 (*hash-rewrites?* T)
+	 (*rewrite-hash* ;;if *hash-rewrites?*
+	  (copy (rewrite-hash ps)))
+	 (sformnums (if (memq sformnum '(* + -))
+			sformnum
+		       (list sformnum)))
+	 (sforms (s-forms goalsequent))
+	 (name (pc-parse name 'name))
+	 (occurrence (if (numberp occurrence)
+			 (list occurrence)
+			 occurrence))
+	 (*max-occurrence* (if (consp occurrence)
+			       (apply #'max occurrence)
+			       0))
+	 (*dependent-decls* nil)
+	 (*if-simplifies* if-simplifies))
+    (cond 
+	  ((not (or (null occurrence)
+		    (and (listp occurrence)
+			 (every #'(lambda (x)
+				    (and (numberp x)
+					 (plusp x)))
+				occurrence))))
+	   (format-if "Occurrence ~a must be nil, a positive number or a 
+list of positive numbers" occurrence)
+	   (values 'X nil nil))
+	  (t 
+	   (let ((new-sforms
+		  (expand-sforms name sforms sformnums
+				 occurrence 
+				 +1 -1 nil)))
+	     (cond ((every #'eq sforms new-sforms)
+		    (values 'X nil nil))
+		   (t (mapcar #'(lambda (x)
+				  (pushnew x (dependent-decls ps)))
+			      *dependent-decls*)
+		      (values '? (list
+				  (list (lcopy goalsequent
+					  's-forms new-sforms)
+					'dependent-decls
+					*dependent-decls*))))))))))
+
+
+(defun expand-sforms (name sforms sformnums occurrence 
+			   pos neg accum)
+  (if (null sforms)
+      (nreverse accum)
+      (let* ((*count-occurrences* 0)
+	     (fmla (formula (car sforms)))
+	     (new-fmla
+	      (if (in-sformnums? (car sforms) pos neg sformnums)
+		  (expand-defn name fmla occurrence)
+		  fmla))
+	     )
+	(if (eq fmla new-fmla)
+	    (expand-sforms name (cdr sforms)
+			   sformnums occurrence
+			   (if (not (not-expr? fmla))
+			       (1+ pos)
+			       pos)
+			   (if (not-expr? fmla)
+			       (1- neg)
+			       neg)
+			   (cons (car sforms) accum))
+	    (multiple-value-bind (sig result)
+		(if (eq *assert-flag* 'none)
+		    (values 'X new-fmla) ;;NSH(1/16/97): Myla Archer
+		        ;;wanted no-simplification option.
+		    (assert-if-inside new-fmla))
+	      (let ((new-sform (lcopy (car sforms)
+				 'formula (if (eq sig 'X)
+					      new-fmla
+					      result))))
+		(if occurrence
+		    (append (nreverse accum)
+			    (cons new-sform (cdr sforms)))
+		    ;;since only one formula is
+		      ;;processed when occurrence is
+			;;given.  
+		  (expand-sforms name (cdr sforms)
+				 sformnums occurrence
+				 (if (not (not-expr? fmla))
+				     (1+ pos)
+				     pos)
+				 (if (not-expr? fmla)
+				     (1- neg)
+				     neg)
+				 (cons new-sform accum)))))))))
+
+(defun match-defns (expr def-axioms)
+  (cond ((null def-axioms) 'fail)
+	(t (let* ((*modsubst* T)
+		  (ax (car def-axioms))
+		  (def-expr (if (forall-expr? ax)
+				(expression ax)
+				ax))
+		  (lhs (args1 def-expr))
+		  (rhs (args2 def-expr))
+		  (sub 
+		   (call-match lhs expr nil nil)))
+	     (if (not (eq sub 'fail))
+		 (values sub rhs)
+		 'fail)))))
+
+
+;;; SO 9/2/94 - Added methods for projection-application and field-application
+
+(defmethod expand-defn (name (expr projection-application) occurrence)
+  (if (and (plusp *max-occurrence*)
+	   (< *max-occurrence* *count-occurrences*))
+      expr
+      (let* ((arg (argument expr))
+	     (newarg (expand-defn name arg occurrence)))
+	(lcopy expr 'argument newarg))))
+
+(defmethod expand-defn (name (expr field-application) occurrence)
+  (if (and (plusp *max-occurrence*)
+	   (< *max-occurrence* *count-occurrences*))
+      expr
+      (let* ((arg (argument expr))
+	     (newarg (expand-defn name arg occurrence)))
+	(lcopy expr 'argument newarg))))
+
+(defmethod expand-defn (name (expr application) occurrence)
+  (if (and (plusp *max-occurrence*)
+	   (< *max-occurrence* *count-occurrences*))
+      expr
+      (let* ((op* (operator* expr)))
+	(if (and (name? op*);; SO - 12-18-92
+		 (same-id op* name)
+		 (null occurrence)
+		 *if-simplifies*)
+	    (let* ((def-axioms (create-formulas (resolution op*))))
+	      (multiple-value-bind
+		  (subst rhs)
+		  (match-defns expr def-axioms)
+		(if (not (eq subst 'fail))
+		    (let* ((xsubst
+			    (loop for (x . y) in subst
+				  collect
+				  (cons x (expand-defn name y occurrence))))
+			   (subexpr  (substit rhs xsubst)))
+		      (multiple-value-bind
+			  (sig value)
+			  (lazy-assert-if  subexpr)
+			(cond ((and (or (typep subexpr 'cases-expr)
+					(branch? subexpr))
+				    (eq sig 'X))
+			       (expand-defn-application name expr
+							occurrence))
+			      (t (pushnew (declaration (resolution op*))
+					  *dependent-decls*)
+				 value))))
+		    (expand-defn-application name expr occurrence))))
+	    (expand-defn-application name expr occurrence)))))
+
+(defmethod expand-defn (name (expr infix-application) occurrence)
+  (let ((nexpr (call-next-method)))
+    (unless (eq nexpr expr)
+      (setf (parens nexpr) 1))
+    nexpr))
+				       
+(defmethod expand-defn-application (name (expr application) occurrence)
+  (let* ((oper (operator expr))
+	 (arg (argument expr))
+	 (op* (operator* expr))
+	 (newoper (expand-defn name oper occurrence))
+	 (newargs (expand-defn name arg occurrence)))
+    (if (and (not (eq oper newoper))
+	     (typep  op* 'name-expr)
+	     (same-id op* name)
+	     (typep newoper 'lambda-expr))
+	(substit (expression newoper)
+	  (pairlis-args (bindings newoper)
+			(argument* newargs)))
+	(let* ((stype (find-supertype (type newoper)))
+	       (nex (lcopy expr
+		      'operator newoper
+		      'argument newargs
+		      'type (if (typep (domain stype) 'dep-binding)
+				(substit (range stype)
+				  (acons (domain stype) newargs nil))
+				(range stype)))))
+	  (unless (eq newoper (operator expr))
+	    (change-application-class-if-necessary expr nex))
+	  nex))))
+
+
+(defmethod expand-defn (name (expr name-expr) occurrence)
+  (if (and (plusp *max-occurrence*)
+	   (< *max-occurrence* *count-occurrences*))
+      expr
+      (cond ((same-id name expr)
+	     (setf *count-occurrences* (1+ *count-occurrences*))
+	     (if (and (or (null occurrence)
+			  (member *count-occurrences* occurrence
+				  :test #'eql))
+		      (def-axiom (declaration expr)))
+		 (let ((rhs (args2
+			     (subst-mod-params
+			      (car (last (def-axiom
+					     (declaration expr))))
+			      (module-instance (resolution expr))))))
+		   (cond ((not *if-simplifies*)
+			  (pushnew (declaration expr)
+				   *dependent-decls*)
+			  rhs)
+			 ((typep rhs 'lambda-expr)
+			  expr)
+			 ((or (typep rhs 'cases-expr)
+			      (branch? rhs))
+			  (multiple-value-bind
+				(sig result)
+			      (lazy-assert-if rhs)
+			    (cond ((eq sig 'X)
+				   expr)
+				  (t (pushnew (declaration expr)
+					      *dependent-decls*)
+				     result))))
+			 (t (pushnew (declaration expr)
+				     *dependent-decls*)
+			    rhs)))
+		 expr))
+	    (t expr))))
+
+(defmethod expand-defn (name (expr binding-expr) occurrence)
+  (if (and (plusp *max-occurrence*)
+	   (< *max-occurrence* *count-occurrences*))
+      expr
+      (lcopy expr
+	'expression (expand-defn name (expression expr) occurrence))))
+
+(defmethod expand-defn (name (expr tuple-expr) occurrence)
+  (if (and (plusp *max-occurrence*)
+	   (< *max-occurrence* *count-occurrences*))
+      expr
+  (lcopy expr
+    'exprs (expand-defn name (exprs expr) occurrence))))
+
+(defmethod expand-defn (name (expr record-expr) occurrence)
+  (if (and (plusp *max-occurrence*)
+	   (< *max-occurrence* *count-occurrences*))
+      expr
+  (lcopy expr
+    'assignments
+    (expand-defn name (assignments expr) occurrence))))
+
+(defmethod expand-defn (name (expr assignment) occurrence)
+  (lcopy expr
+    'arguments (expand-defn name (arguments expr) occurrence)
+    'expression (expand-defn name (expression expr) occurrence)))
+
+(defmethod expand-defn (name (expr list) occurrence)
+  (let ((newlist (mapcar #'(lambda (x) (expand-defn name x
+						    occurrence))
+			 expr)))
+    (if (every #'eq expr newlist)
+	expr
+	newlist)))
+
+(defmethod expand-defn (name (expr update-expr) occurrence)
+  (if (and (plusp *max-occurrence*)
+	   (< *max-occurrence* *count-occurrences*))
+      expr
+  (lcopy expr
+    'expression (expand-defn name (expression expr) occurrence)
+    'assignments (expand-defn name (assignments expr) occurrence))))
+
+(defmethod expand-defn (name (expr cases-expr) occurrence)
+  (if (and (plusp *max-occurrence*)
+	   (< *max-occurrence* *count-occurrences*))
+      expr
+  (lcopy expr
+    'expression (expand-defn name (expression expr) occurrence)
+    'selections (expand-defn name (selections expr) occurrence)
+    'else-part (expand-defn name (else-part expr) occurrence))))
+
+(defmethod expand-defn (name (expr selection) occurrence)
+  (lcopy expr
+    'expression (expand-defn name (expression expr) occurrence)))
+
+(defmethod expand-defn (name (expr expr) occurrence)
+  (declare (ignore name occurrence))
+  expr)
---
+++ src/prover/freevars.lisp
@@ -0,0 +1,282 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; -*- Mode: Lisp -*- ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; freevars.lisp -- 
+;; Author          : Sam Owre & N. Shankar
+;; Created On      : Wed Aug 17 00:48:46 1994
+;; Last Modified By: Sam Owre
+;; Last Modified On: Sat Oct 31 02:38:17 1998
+;; Update Count    : 2
+;; Status          : Alpha test
+;; 
+;; HISTORY
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+ (in-package :pvs)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;collect free variables
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; From Sam: 
+;;; freevars - returns the free variables of an expression - a list of
+;;; name-exprs.  The
+;;; returned list has no duplication, and the original expression is not
+;;; modified.  Note that the formula must be typechecked, as this is the
+;;; only way to determine whether a name is a variable.
+
+(defmacro fv-union (fv1 fv2)
+  `(union ,fv1 ,fv2 :test #'same-declaration))
+
+(defun freevars (obj)
+;;  (sort (freevars* obj  frees)
+;;	#'(lambda (x y) (occurs-in x (type y))))
+  (freevars* obj nil))
+
+(defmethod freevars* :around ((expr expr) frees)
+  (with-slots (free-variables) expr
+    (if (eq free-variables 'unbound)
+	(call-next-method)
+	(if frees
+	    (append (set-difference free-variables frees
+				    :test #'same-declaration)
+		    frees)
+	    free-variables))))
+
+(defmethod freevars* :around ((texpr type-expr) frees) 
+  (with-slots (free-variables) texpr
+    (if (eq free-variables 'unbound)
+	(call-next-method)
+	(remove-duplicates (append free-variables frees)
+	  :test #'same-declaration))))
+
+(defmethod freevars* ((expr projection-application) frees)
+  (let ((efrees (freevars* (argument expr) nil)))
+    (setf (free-variables expr) efrees)
+    (fv-union efrees frees)))
+
+(defmethod freevars* ((expr field-application) frees)
+  (let ((efrees (freevars* (argument expr) nil)))
+    (setf (free-variables expr) efrees)
+    (fv-union efrees frees)))
+
+(defmethod freevars* ((expr application) frees)
+  (let* ((ofrees (freevars* (operator expr) nil))
+	 (afrees (freevars* (argument expr) nil))
+	 (efrees (fv-union ofrees afrees)))
+    (setf (free-variables expr) efrees)
+    (fv-union efrees frees)))
+
+(defmethod freevars* ((list list) frees)
+  (freevars-list list frees))
+
+(defun freevars-list (list frees)
+  (if (null list)
+      frees
+      (let* ((frees-car (freevars* (car list) frees))
+	     (frees-cdr (freevars-list (cdr list) frees-car)))
+	(if (and (typep (car list) '(or binding field-decl))
+		 (not (member (car list) frees-car :test #'same-declaration)))
+	    (remove (car list) frees-cdr :test #'same-declaration)
+	    frees-cdr))))
+
+
+(defmethod declaration ((expr field-decl))
+  expr)
+(defmethod declaration ((expr dep-binding))
+  expr)
+
+(defmethod freevars* ((expr bind-decl) frees)
+  (freevars* (type expr) frees))
+
+(defmethod no-freevars? ((expr bind-decl))
+  (no-freevars? (type expr)))
+
+(defmethod freevars* ((expr binding-expr) frees)
+  (let* ((frees-expression (freevars* (expression expr) nil))
+	 (frees-bindlist (freevars* (bindings expr) nil))
+	 (diff (set-difference frees-expression (bindings expr)
+			       :test #'same-declaration))
+	 (nfrees (fv-union frees-bindlist diff)))
+    (setf (free-variables expr) nfrees)
+    (fv-union nfrees frees)))
+
+(defmethod freevars* ((expr name-expr) frees)
+  (with-slots (kind type resolutions free-variables) expr
+    (assert kind)
+    (cond ((variable? expr)
+	   (let* ((tfrees (freevars* type nil))
+		  (ufrees (fv-union tfrees frees)))
+	     (setf free-variables (cons expr tfrees))
+	     (if (not (member expr ufrees :test #'same-declaration))
+		 (cons expr ufrees)
+		 ufrees)))
+	  (t (let ((afrees (freevars* (actuals (module-instance
+						(car resolutions)))
+			     nil)))
+	       (setf free-variables afrees)
+	       (fv-union afrees frees))))))
+
+(defmethod freevars* ((res resolution) frees)
+  (with-slots (module-instance) res
+    (fv-union (freevars* (actuals module-instance) nil) frees)))
+
+(defmethod freevars* ((expr field-name-expr) frees)
+  (assert (kind expr))
+  (cond ((variable? expr)
+	 (let* ((tfrees (freevars* (type expr) nil))
+		(ufrees (fv-union tfrees frees)))
+	   (setf (free-variables expr) (cons expr tfrees))
+	   (if (not (member expr ufrees :test #'same-declaration))
+	       (cons expr ufrees)
+	       ufrees)))
+	(t (let* ((afrees (freevars* (actuals (module-instance
+					       (resolution expr)))
+				     nil))
+		  (tfrees (freevars* (type expr) nil))
+		  (ufrees (fv-union afrees tfrees)))
+	     (setf (free-variables expr) ufrees)
+	     (fv-union frees ufrees)))))
+
+(defmethod freevars* ((expr projection-expr) frees)
+  (let ((tfrees (freevars* (type expr) nil)))
+    (setf (free-variables expr) tfrees)
+    (fv-union tfrees frees)))
+
+(defmethod freevars* ((expr record-expr) frees)
+  (let ((rfrees (freevars* (assignments expr) nil)))
+    (setf (free-variables expr) rfrees)
+    (fv-union rfrees frees)))
+
+(defmethod freevars* ((expr tuple-expr) frees)
+  (let ((efrees (freevars* (exprs expr) nil)))
+    (setf (free-variables expr) efrees)
+    (fv-union efrees frees)))
+
+(defmethod freevars* ((expr update-expr) frees)
+  (let* ((efrees (freevars* (expression expr) nil))
+	 (afrees (freevars* (assignments expr) nil))
+	 (tfrees (freevars* (type expr) nil))
+	 (ufrees (fv-union efrees (fv-union afrees tfrees))))
+    (setf (free-variables expr) ufrees)
+    (fv-union ufrees frees)))
+
+(defmethod freevars* ((expr assignment) frees)
+  (fv-union (fv-union (freevars* (expression expr) nil)
+		      (freevars* (arguments expr) nil))
+	    frees))
+
+(defmethod freevars* ((expr field-assignment-arg) frees)
+  frees)
+
+(defmethod no-freevars? ((expr assignment))
+  (and (no-freevars? (arguments expr))
+       (no-freevars? (expression expr))))
+
+(defmethod freevars* ((expr cases-expr) frees)
+  (let* ((tfrees (freevars* (type expr) nil))
+	 (sfrees (freevars* (selections expr) nil))
+	 (elfrees (freevars* (else-part expr) nil))
+	 (exfrees (freevars* (expression expr) nil))
+	 (efrees (fv-union tfrees
+			   (fv-union sfrees (fv-union elfrees exfrees)))))
+    (setf (free-variables expr) efrees)
+    (fv-union efrees frees)))
+
+(defmethod freevars* ((expr selection) frees)
+  (set-difference (freevars* (expression expr) frees)
+		  (args expr)
+		  :test #'same-declaration))
+
+(defmethod no-freevars? ((expr selection))
+  (no-freevars? (expression expr)))
+
+(defmethod freevars* ((expr actual) frees)
+  (if (type-value expr)
+      (freevars* (type-value expr) frees)
+      (freevars* (expr expr) frees)))
+
+(defmethod no-freevars? ((expr actual))
+  (and (no-freevars? (expr expr))
+       (no-freevars? (type-value expr))))
+
+(defmethod freevars* ((expr expr) frees)
+  frees)
+
+(defmethod no-freevars? ((list list))
+  (every@ #'no-freevars? list))
+
+(defmethod freevars* ((mn modname) frees)
+  (let ((afrees (freevars* (actuals mn) nil)))
+    (fv-union afrees frees)))
+
+(defmethod freevars* ((texpr type-name) frees)
+  (let* ((act (actuals (module-instance (resolution texpr))))
+	 (afrees (freevars* act nil)))
+    (setf (free-variables texpr) afrees)
+    (fv-union afrees frees)))
+
+(defmethod freevars* ((texpr type-application) frees)
+  (let* ((tfrees (freevars* (type texpr) nil))
+	 (pfrees (freevars* (parameters texpr) nil))
+	 (nfrees (fv-union tfrees pfrees)))
+    (setf (free-variables texpr) nfrees)
+    (fv-union nfrees frees)))
+
+(defmethod freevars* ((texpr subtype) frees)
+  (let* ((tfrees (freevars* (supertype texpr) nil))
+	 (pfrees (freevars* (predicate texpr) nil))
+	 (ufrees (fv-union tfrees pfrees)))
+    (setf (free-variables texpr) ufrees)
+    (fv-union ufrees frees)))
+
+(defmethod freevars* ((texpr expr-as-type) frees)
+  (if (predicate texpr)
+      (call-next-method)
+      (let ((tfrees (freevars* (expr texpr) nil)))
+	(setf (free-variables texpr) tfrees)
+	(fv-union tfrees frees))))
+
+(defmethod freevars* ((texpr funtype) frees)
+  (with-slots (domain range) texpr
+    (let* ((dfrees (freevars* domain nil))
+	   (rfrees (freevars* range nil))
+	   (ufrees (fv-union dfrees rfrees))
+	   (tfrees (if (typep domain 'dep-binding)
+		       (remove-if #'(lambda (d) (eq (declaration d) domain))
+			 ufrees)
+		       ufrees)))
+      (setf (free-variables texpr) tfrees)
+      (fv-union tfrees frees))))
+
+(defmethod freevars* ((texpr tupletype) frees)
+  (let ((tfrees (freevars* (types texpr) nil)))
+    (setf (free-variables texpr) tfrees)
+    (fv-union tfrees frees)))
+
+(defmethod freevars* ((texpr recordtype) frees)
+  (let ((ffrees (freevars* (fields texpr) nil))) 
+    (setf (free-variables texpr) ffrees)
+    (fv-union ffrees frees)))
+
+(defmethod freevars* ((texpr field-decl) frees)
+  (freevars* (type texpr) frees))
+
+(defmethod no-freevars? ((texpr field-decl))
+  (no-freevars? (type texpr)))
+
+(defmethod freevars* ((te dep-binding) frees)
+  (freevars* (type te) frees))
+
+(defmethod freevars* ((sym symbol) frees)
+  frees)
+
+(defmethod no-freevars? ((texpr dep-binding))
+  (no-freevars? (type texpr)))
+
+(defmethod no-freevars? ((texpr type-expr))
+  (with-slots (free-variables) texpr
+    (and (not (eq free-variables 'unbound))
+	 (null free-variables))))
+
+(defmethod no-freevars? ((expr expr))
+  (with-slots (free-variables) expr
+    (and (not (eq free-variables 'unbound))
+	 (null free-variables))))
