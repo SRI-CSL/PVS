ebd8e80853a7f533212ec6a8f07eeae83d6f9fa9
owre <owre@ab98feae-9515-0410-836f-c1e8bd48b6aa>
1999-01-06 20:02:48 +0000

    Adding bdd_vfuns files
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@301 ab98feae-9515-0410-836f-c1e8bd48b6aa

---
+++ BDD/bdd/src/bdd_vfns.h
@@ -0,0 +1,23 @@
+/*
+ DOCUMENTATION INFORMATION				         module: BDD
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ system	   : Apollo DN3000, HP 9000/S500, IBM RS/6000
+ file	   : bdd_vfns.h
+ unit-title: 
+ ref.	   : Efficient Implementation of a BDD Package, Karl S. Brace. DAC'90
+ author(s) : Copyright (c) 1990-1998 G.L.J.M. Janssen
+ date	   : 29-JAN-1998
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+*/
+
+#define BDDVEC_SIZE(F)			((F) ? ((int *) F)[-1] : 0)
+
+extern BDDPTR *MakeBDDVec		(int size);
+extern   void  FreeBDDVec		(BDDPTR *F);
+extern   void  FreeBDDVec2		(BDDPTR *F, int size);
+extern BDDPTR *CopyBDDVec		(BDDPTR *F);
+extern BDDPTR *CopyBDDVec2		(BDDPTR *F, int size, BDDPTR *toF);
+extern   void  ComplBDDVec2             (BDDPTR *F, register int size);
+extern   void  ComplBDDVec              (BDDPTR *F);
+
+extern BDDPTR *bdd_constrain_vec	(BDDPTR *F, int i, int j, BDDPTR c);
