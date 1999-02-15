5aca18899ac1cfe6de988f8fc24860202276d02e
owre <owre@ab98feae-9515-0410-836f-c1e8bd48b6aa>
1999-02-15 22:10:54 +0000

    Adding pvs-eval.el
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@520 ab98feae-9515-0410-836f-c1e8bd48b6aa

---
+++ emacs/emacs-src/pvs-eval.el
@@ -0,0 +1,57 @@
+; PVS evaluator - emacs end.  dave_sc 10/12/98
+
+(defpvs pvs-lisp-theory typecheck (theoryname)
+  "Generates the Lisp code for a given theory and displays it in a buffer"
+  (interactive (complete-theory-name "Generate lisp for theory: "))
+  (popper-bury-output t)
+  (message "Generating Lisp for theory...")
+  (pvs-send-and-wait (format "(generate-lisp-for-theory \"%s\")"
+			 theoryname) nil nil 'dont-care)
+  (message "")
+  (let ((buf (pvs-find-lisp-file theoryname)))
+    (when buf
+      (save-excursion
+	(set-buffer buf)
+	(setq pvs-context-sensitive t)
+	(lisp-mode)))))
+
+(defun pvs-find-lisp-file (theoryname)
+  (let ((buf (get-buffer (format "%s.lisp" theoryname))))
+    (when buf
+      (kill-buffer buf)))
+  (let ((lisp-file (format "%s%s.lisp" *pvs-current-directory* theoryname)))
+    (when (file-exists-p lisp-file)
+      (find-file-read-only-other-window lisp-file))))
+
+
+(defpvs pvs-lisp-eval prove (theory)
+  "Invokes the ground evaluator in the context of the given PVS theory"
+  (interactive (complete-theory-name "Use context of theory: "))
+  (confirm-not-in-checker)
+  (pvs-evaluator-busy)
+  (save-some-pvs-buffers)
+  (popper-bury-output t)
+  (ilisp-send (format "(evaluation-mode \"%s\")" theory) nil 'pr t))
+
+(defvar pvs-in-evaluator nil)
+
+(defun confirm-not-in-checker ()
+  (when (or pvs-in-checker pvs-in-evaluator)
+    (unless (equal (buffer-name) ilisp-buffer)
+      (pop-to-buffer ilisp-buffer)
+      (goto-char (point-max)))
+    (error (format "Must exit the %s first"
+	       (if pvs-in-checker "checker" "evaluator")))))
+
+(defun pvs-evaluator-busy ()
+  (setq pvs-in-evaluator t)
+  (pvs-busy))
+
+(defun pvs-evaluator-ready ()
+  (setq pvs-in-evaluator nil)
+  (pvs-ready))
+
+
+
+
+
