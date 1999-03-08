4e35b521a7a70d249ac829dd63b4583dbd60bfb0
dave_sc <dave_sc@ab98feae-9515-0410-836f-c1e8bd48b6aa>
1999-03-08 19:33:15 +0000

    added a unwind-protect so that exiting with :reset etc does the right thing and lets emacs know we're no longer busy.
    removed references to the symbolic evaluator
    
    added a help command to the repl.
    
    
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@677 ab98feae-9515-0410-836f-c1e8bd48b6aa

--- src/groundeval/generate-lisp-for-theory.lisp
+++ src/groundeval/generate-lisp-for-theory.lisp
@@ -7,83 +7,84 @@
 	  (t (pvs2cl-theory theory)
 	     (print-lisp-defns theoryname (format nil "~a.lisp" theoryname) T)))))
 
-(defun evaluation-mode (theoryname &optional symbolic?)
+(defun evaluation-mode (theoryname)
   (let ((theory (get-theory theoryname)))  
     (if theory
-      (let ((*current-theory* theory)
-	    (*generate-tccs* 'none)
-	    (*current-context* (or (saved-context theory)
-				   (context nil)))
-	    (*in-evaluator* t))
-	(format t "~%~%PVS Ground Evaluation.~%Enter a ground expression in quotes at the <GndEval> prompt~%")
-	(evaluate symbolic?))
-      (pvs-message "Theory ~a is not typechecked" theoryname)))
+	(unwind-protect
+	    (let ((*current-theory* theory)
+		  (*generate-tccs* 'none)
+		  (*current-context* (or (saved-context theory)
+					 (context nil)))
+		  (*in-evaluator* t))
+	      (format t "~%~%PVS Ground Evaluation.~%Enter a ground expression in quotes at the <GndEval> prompt~%")
+	      (evaluate))
+	  (pvs-emacs-eval "(pvs-evaluator-ready)"))
+	(pvs-message "Theory ~a is not typechecked" theoryname)))
   (pvs-emacs-eval "(pvs-evaluator-ready)"))
 
-(defun gqread (prompt)
-  (format t "~%~a"  prompt)
+(defun gqread (&optional prompt)
+  (format t "~%<GndEval> ")
   (force-output)
   (let ((input (ignore-errors (read))))
     (cond ((member input '(quit q exit (quit)(exit)(q))
 		   :test #'equal)
 	   (if (pvs-y-or-n-p "~%Do you really want to quit?  ")
 	       (throw 'quit nil)
-	       (gqread prompt)))
+	       (gqread)))
 	  ((and (consp input)
 		(member (car input) '(lisp pvs::lisp)))  ;; allow for us to be interrupted
 	   (format t "~%~s~%" (eval (cadr input)))
-	   (gqread prompt))
+	   (gqread))
+	  ((member input '(h help)
+		   :test #'equal)
+	   (format t "~%Evaluator commands:~%")
+	   (format t "  h,help         : print this message~%")
+	   (format t "  q,quit         : exit the evaluator~%")
+	   (format t "  timing         : print timing information for each evaluation~%")
+	   (format t "  notiming       : turn off printing of timing information~%")
+	   (format t "  destructive    : use destructive evaluation where possible~%")
+	   (format t "  nondestructive : don't use destructive evaluation~%")
+	   (format t "~%~%Evaluator input should be enclosed in double quotes~%")
+	   (gqread))
 	  ((eq input 'abort)
 	   (if (pvs-y-or-n-p "~%Do you really want to abort?  ")
 	       (throw 'abort T)
-	       (gqread prompt)))
+	       (gqread)))
 	  ((eq input 'timing)
 	   (setq *pvs-eval-do-timing* t)
-	   (gqread prompt))
+	   (gqread))
 	  ((eq input 'notiming)
 	   (setq *pvs-eval-do-timing* nil)
-	   (gqread prompt))
+	   (gqread))
 	  ((eq input 'destructive)
 	   (setq *destructive?* t)
-	   (gqread prompt))
+	   (gqread))
 	  ((eq input 'nondestructive)
 	   (setq *destructive?* nil)
-	   (gqread prompt))
-	  ((eq input 'ground)
-	   (evaluate)
-	   (throw 'quit nil))
-	;  ((eq input 'symbolic)
-	;   (evaluate t)
-	;   (throw 'quit nil))
+	   (gqread))
 	  (t
 	   input))))
 
-(defun evalprompt (&optional symbolic?)
-  (if symbolic?
-      "<SymEval> "
-      "<GndEval> "))
-
-(defun evaluate (&optional symbolic?)
+(defun evaluate ()
   (let ((result
 	 (catch 'abort
 	   (catch 'quit
 	     (catch 'tcerror
-	       (let* ((input (ignore-errors (gqread (evalprompt symbolic?))))
+	       (let* ((input (ignore-errors (gqread)))
 		      (pr-input (pc-parse input 'expr))
 		      (tc-input (pc-typecheck pr-input)))
 		 (if (ground-type? (type tc-input))
 		     (format t "~%==> ~%  ~a"
 		       (if *pvs-eval-do-timing*
-			   (time (if symbolic?
-				     (norm tc-input)
-				     (eval (pvs2cl tc-input))))
-			   (eval (if symbolic?
-				     (norm tc-input)
-				     (pvs2cl tc-input)))))
+			   (time (eval (pvs2cl tc-input)))
+			   (eval (pvs2cl tc-input))))
 		     (format t "~%Not a ground expression. Cannot evaluate"))
 		 T))))))
-	(when result
-	  (evaluate symbolic?))))
+    (when result
+      (evaluate))))
+
+
+
 
 
 
