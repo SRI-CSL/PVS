7f40533a8ae2681ab9e3c7e303f84dacf8a4812f
ruess <ruess@ab98feae-9515-0410-836f-c1e8bd48b6aa>
1999-03-13 01:02:35 +0000

    new def-pvs-terms
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@716 ab98feae-9515-0410-836f-c1e8bd48b6aa

--- src/WS1S/lisp/signature.lisp
+++ src/WS1S/lisp/signature.lisp
@@ -12,7 +12,15 @@
 (def-pvs-term add-to-fset "add[nat]" "record_prelude_example"
                           :expected "[nat, finite_set[nat] -> finite_set[nat]]")
 
-
+(def-pvs-term the1      "the[nat]" "naturalnumbers")
+(def-pvs-term the2      "the[finite_set[nat]]" "record_prelude_example")
+(def-pvs-term minus1 "-" "naturalnumbers" :expected "[nat, nat -> nat]")
+(def-pvs-term plus1  "+" "naturalnumbers" :expected "[nat, nat -> nat]")
+(def-pvs-term gt1  ">"  "naturalnumbers" :expected "[nat, nat -> boolean]")
+(def-pvs-term ge1    ">=" "naturalnumbers" :expected "[nat, nat -> boolean]")
+(def-pvs-term lt1   "<" "naturalnumbers" :expected "[nat, nat -> boolean]")
+(def-pvs-term le1    "<=" "naturalnumbers" :expected "[nat, nat -> boolean]")
+                       
 
 (defun ws1s-types ()
   (list *boolean*
@@ -26,17 +34,16 @@
   (subtype-of? (type expr) *boolean*))
 
 (defun 1st-order? (expr)
+  (assert (typep expr 'expr))
   (or (subtype-of? (type expr) *naturalnumber*)
-      (some #'subtype-of-nat? (judgement-types+ expr))
-      (some #'subtype-of-nat? (gethash expr *known-judgements*))))
+      (some #'subtype-of-nat? (judgement-types+ expr))))
 
 (defun subtype-of-nat? (type)
   (subtype-of? type *naturalnumber*))
   
 (defun 2nd-order? (expr)
   (or (subtype-of? (type expr) (fset-of-nats))
-      (finite-set-of-nat? expr)
-      (some #'finite-set-of-nat? (gethash expr *known-judgements*))))
+      (finite-set-of-nat? expr)))
  
 (defun level (expr)
   (cond ((boolean? expr) 0)
