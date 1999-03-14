b2e928ffe0f5e4b3b0b5c763060581947816bfc2
owre <owre@ab98feae-9515-0410-836f-c1e8bd48b6aa>
1999-03-14 10:12:19 +0000

    Modified so that exit codes are properly generated.
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@725 ab98feae-9515-0410-836f-c1e8bd48b6aa

--- src/make-pvs.lisp
+++ src/make-pvs.lisp
@@ -12,78 +12,85 @@
 
 (defvar *runtime* nil)
 
-(excl:generate-application
- "pvs-allegro5.0"
- (format nil "./bin/~a/~a/"
-   (or (sys:getenv "PLATFORM") "ix86-redhat5")
-   (if *runtime* "runtime" "full"))
- (list "./src/make-allegro-pvs.lisp")
- :additional-arguments nil
- :additional-forms nil
- :additional-plus-arguments nil
- :autoload-warning t
- ;; :bundle-file nil
- ;; :c-heap-size nil
- ;; :c-heap-start nil
- :case-mode :case-insensitive-upper
- ;; :copy-shared-libraries nil
- :debug-on-error t
- ;; :destination-directory nil
- :discard-arglists nil
- :discard-compiler nil
- :discard-local-name-info nil
- :discard-source-file-info nil
- :discard-xref-info nil
- ;; :dribble-file nil
- :dst t
- :exit-after-image-build t
- :generate-fonts nil
- :include-clim nil
- ;; :include-common-graphics nil ;Windows
- :include-compiler t
- :include-composer nil
- :include-debugger t
- :include-devel-env t
- ;; :include-ide nil ;Windows
- :include-tpl t
- :include-xcw nil
- :internal-debug nil
- :lisp-heap-size 20000000
- :lisp-heap-start nil
- ;; :lisp-files nil
- :load-local-names-info nil
- :load-source-file-info t
- :load-xref-info t
- :newspace 4000000
- :oldspace 512000
- :opt-debug 1
- :opt-safety 1
- :opt-space 1
- :opt-speed 3
- ;; :pll-file nil
- ;; :pll-from-sys nil
- :post-load-form nil
- :pre-dump-form nil
- :pre-load-form nil
- :preserve-documentation-strings t
- :presto nil
- :presto-flush-to-code-file nil
- ;; :presto-lib nil
- ;; :print-startup-message t
- :read-init-files nil
- :record-source-file-info t
- :record-xref-info t
- :restart-app-function nil
- :restart-init-function nil
- :runtime nil
- :server-name nil
- ;; :show-window nil ;Windows
- ;; :splash-from-file nil ;Windows
- :temporary-directory "/usr/tmp/"
- :us-government nil
- ;;    :user-shared-libraries t
- :verbose t
- ;; :wait nil ;Windows
- )
-
-(excl:exit)
+(let ((exitcode 1))
+  (unwind-protect
+      (multiple-value-bind (v err)
+	  (ignore-errors
+	    (excl:generate-application
+	     "pvs-allegro5.0"
+	     (format nil "./bin/~a/~a/"
+	       (or (sys:getenv "PLATFORM") "ix86-redhat5")
+	       (if *runtime* "runtime" "full"))
+	     (list "./src/make-allegro-pvs.lisp")
+	     :additional-arguments nil
+	     :additional-forms nil
+	     :additional-plus-arguments nil
+	     :autoload-warning t
+	     ;; :bundle-file nil
+	     ;; :c-heap-size nil
+	     ;; :c-heap-start nil
+	     :case-mode :case-insensitive-upper
+	     ;; :copy-shared-libraries nil
+	     :debug-on-error t
+	     ;; :destination-directory nil
+	     :discard-arglists nil
+	     :discard-compiler nil
+	     :discard-local-name-info nil
+	     :discard-source-file-info nil
+	     :discard-xref-info nil
+	     ;; :dribble-file nil
+	     :dst t
+	     :exit-after-image-build t
+	     :generate-fonts nil
+	     :include-clim nil
+	     ;; :include-common-graphics nil ;Windows
+	     :include-compiler t
+	     :include-composer nil
+	     :include-debugger t
+	     :include-devel-env t
+	     ;; :include-ide nil ;Windows
+	     :include-tpl t
+	     :include-xcw nil
+	     :internal-debug nil
+	     :lisp-heap-size 20000000
+	     :lisp-heap-start nil
+	     ;; :lisp-files nil
+	     :load-local-names-info nil
+	     :load-source-file-info t
+	     :load-xref-info t
+	     :newspace 4000000
+	     :oldspace 512000
+	     :opt-debug 1
+	     :opt-safety 1
+	     :opt-space 1
+	     :opt-speed 3
+	     ;; :pll-file nil
+	     ;; :pll-from-sys nil
+	     :post-load-form nil
+	     :pre-dump-form nil
+	     :pre-load-form nil
+	     :preserve-documentation-strings t
+	     :presto nil
+	     :presto-flush-to-code-file nil
+	     ;; :presto-lib nil
+	     ;; :print-startup-message t
+	     :read-init-files nil
+	     :record-source-file-info t
+	     :record-xref-info t
+	     :restart-app-function nil
+	     :restart-init-function nil
+	     :runtime nil
+	     :server-name nil
+	     ;; :show-window nil ;Windows
+	     ;; :splash-from-file nil ;Windows
+	     :temporary-directory "/usr/tmp/"
+	     :us-government nil
+	     ;;    :user-shared-libraries t
+	     :verbose t
+	     ;; :wait nil ;Windows
+	     ))
+	(if err
+	    (let ((*print-readably* nil))
+	      (format t "~a" err))
+	    (setq exitcode 0)))
+    (excl:exit exitcode)))
