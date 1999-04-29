0f9699c4307a12ed3ddeced21c27a0c62aa5c3b3
owre <owre@ab98feae-9515-0410-836f-c1e8bd48b6aa>
1999-04-29 10:08:14 +0000

    Referenced the functions defined in utils-ld-table.
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@1042 ab98feae-9515-0410-836f-c1e8bd48b6aa

--- src/utils/file-utils.lisp
+++ src/utils/file-utils.lisp
@@ -1,23 +1,29 @@
 (in-package 'pvs)
 (require :foreign)
 
-(ff:def-foreign-call file_exists_p ((filename (* :char) string))
+(ff:def-foreign-call fileutils___file_exists_p
+    ((filename (* :char) string))
   :returning :int)
 
-(ff:def-foreign-call directory_p ((filename (* :char) string))
+(ff:def-foreign-call fileutils___directory_p
+    ((filename (* :char) string))
   :returning :int)
 
-(ff:def-foreign-call read_permission_p ((filename (* :char) string))
+(ff:def-foreign-call fileutils___read_permission_p
+    ((filename (* :char) string))
   )
 
-(ff:def-foreign-call write_permission_p ((filename (* :char) string))
+(ff:def-foreign-call fileutils___write_permission_p
+    ((filename (* :char) string))
   )
 
-(ff:def-foreign-call file_write_time ((filename (* :char) string))
+(ff:def-foreign-call fileutils___file_write_time
+    ((filename (* :char) string))
   :returning :int)
 
-(ff:def-foreign-call getfileinfo ((filename (* :char) string)
-				  (stat (* :int) (array fixnum)))
+(ff:def-foreign-call fileutils___getfileinfo
+    ((filename (* :char) string)
+     (stat (* :int) (array fixnum)))
   )
 
 ;;;
@@ -26,29 +32,32 @@
   `(excl::tilde-expand-unix-namestring (namestring ,filename)))
 
 (defun file-exists-p (filename)
-  (zerop (file_exists_p (expanded-tilde-namestring filename))))
+  (zerop (fileutils___file_exists_p (expanded-tilde-namestring filename))))
 
 (defun directory-p (filename)
   (let* ((filestring (expanded-tilde-namestring filename)))
-    (unless (zerop (directory_p filestring))
+    (unless (zerop (fileutils___directory_p filestring))
       (pathname (if (char= (char filestring (1- (length filestring))) #\/)
 		    filestring
 		    (concatenate 'string filestring "/"))))))
 
 (defun read-permission? (filename)
-  (let ((errno (read_permission_p (expanded-tilde-namestring filename))))
+  (let ((errno (fileutils___read_permission_p
+		(expanded-tilde-namestring filename))))
     (or (zerop errno)
 	(values nil (ff:char*-to-string errno)))))
 
 (defun write-permission? (filename)
-  (let ((errno (write_permission_p (expanded-tilde-namestring filename))))
+  (let ((errno (fileutils___write_permission_p
+		(expanded-tilde-namestring filename))))
     (or (zerop errno)
 	(values nil (ff:char*-to-string errno)))))
 
 (defconstant u1970 (encode-universal-time 0 0 0 1 1 1970 0))
 
 (defun file-write-time (filename)
-  (let ((date (file_write_time (expanded-tilde-namestring filename))))
+  (let ((date (fileutils___file_write_time
+	       (expanded-tilde-namestring filename))))
     (unless (zerop date)
       (+ date u1970))))
 
@@ -56,8 +65,8 @@
 (let ((fstat-array
        (make-array 2 :initial-element 0 :element-type 'fixnum)))
   (defun get-file-info (filename)
-    (when (zerop (getfileinfo (expanded-tilde-namestring filename)
-			      fstat-array))
+    (when (zerop (fileutils___getfileinfo (expanded-tilde-namestring filename)
+					  fstat-array))
       (values (aref fstat-array 0)
 	      (aref fstat-array 1)))))
   
