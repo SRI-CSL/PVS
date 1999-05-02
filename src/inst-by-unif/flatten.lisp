cea55dcaff761c5950e704871cb8c7a675a04256
ruess <ruess@ab98feae-9515-0410-836f-c1e8bd48b6aa>
1999-05-02 22:10:33 +0000

    Put out label stuff.
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@1086 ab98feae-9515-0410-836f-c1e8bd48b6aa

--- src/inst-by-unif/flatten.lisp
+++ src/inst-by-unif/flatten.lisp
@@ -3,7 +3,7 @@
 ;; Propositional flattening, relabeling, and categorization
 
 (defun flatten (ha hs hn ca cs cn)
-  (format t "~%Flatten")
+  (assert (every #'node-p (append ha hs hn ca cs cn)))
   (flatten* ha hs hn ca cs cn))
 
 (defun flatten* (ha hs hn ca cs cn)
@@ -16,47 +16,42 @@
 
 (defun flatten+ (ha hs hn ca cs cn)
   (assert (consp cn))
-  (let* ((c1 (car cn))
-	 (c  (fmla-of c1))
-	 (l  (label-of c1))
+  (let* ((c (car cn))
 	 (cn (cdr cn)))
     (cond ((negation-p c)
-	   (flatten* ha hs (cons (mk-fmla l (arg 1 c)) hn)
+	   (flatten* ha hs (cons (arg 1 c) hn)
 		     ca cs cn))
 	  ((disjunction-p c)
 	   (flatten* ha hs hn
-		     ca cs (cons (mk-fmla (once l) (lhs c))
-				 (cons (mk-fmla (twice l) (rhs c))
-				       cn))))
+		     ca cs (cons (lhs c)
+				 (cons (rhs c) cn))))
 	  ((implication-p c)
-	   (flatten* ha hs (cons (mk-fmla (once l) (lhs c)) hn)
-		     ca cs (cons (mk-fmla (twice l) (rhs c)) cn)))
+	   (flatten* ha hs (cons (lhs c) hn)
+		     ca cs (cons (rhs c) cn)))
 	  ((conjunction-p c)
 	   (flatten* ha hs hn
-		     ca (cons c1 cs) cn))
+		     ca (cons c cs) cn))
 	  (t
 	   (flatten* ha hs hn
-		     (cons c1 ca) cs cn)))))
+		     (cons c ca) cs cn)))))
 
 (defun flatten- (ha hs hn ca cs cn)
   (assert (consp hn))
-  (let* ((h1 (car hn))
-	 (h (fmla-of h1))
-	 (l (label-of h1))
+  (let* ((h (car hn))
 	 (hn (cdr hn)))
     (cond ((negation-p h)
 	   (flatten* ha hs hn
-		     ca cs (cons (mk-fmla l (arg 1 h)) cn)))
+		     ca cs (cons (arg 1 h) cn)))
 	  ((conjunction-p h)
-	   (flatten* ha hs (cons (mk-fmla (once l) (lhs h))
-				 (cons (mk-fmla (twice l) (rhs h))
-				      hn))
+	   (flatten* ha hs (cons (lhs h)
+				 (cons (rhs h) hn))
 		    ca cs cn))
 	  ((or (disjunction-p h)
 	       (implication-p h))
-	   (flatten* ha (cons h1 hs) hn
+	   (flatten* ha (cons h hs) hn
 		     ca cs cn))
 	  (t
-	   (flatten* (cons h1 ha) hs hn
+	   (flatten* (cons h ha) hs hn
 		     ca cs cn)))))
+
 
