021ea343af593b577bf77c6d6a4c729dac0e2226
ruess <ruess@ab98feae-9515-0410-836f-c1e8bd48b6aa>
1999-05-04 01:10:57 +0000

    corrected bug in collection of subst-scores in gensubsts-sequents
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@1105 ab98feae-9515-0410-836f-c1e8bd48b6aa

--- src/inst-by-unif/gensubsts.lisp
+++ src/inst-by-unif/gensubsts.lisp
@@ -12,14 +12,14 @@
   subst
   score)
 
-(defun empty-score-subst ()
+(defvar *empty-score-subst*
   (make-score-subst :subst nil :score 0))
 
-(defun subst-of (s) (score-subst-subst s))
+(defun substitution-of (s) (score-subst-subst s))
 (defun score-of (s) (score-subst-score s))
 
-(defun score-subst= (score-subst1 score-subst2)
-  (and (subst= (subst-of s1) (subst-of s2))
+(defun score-subst= (s1 s2)
+  (and (subst= (substitution-of s1) (substitution-of s2))
        (equal (score-of s1) (score-of s2))))
 
 ;; Top-Level
@@ -32,23 +32,27 @@
     (multiple-value-bind (ha hn ca cn)
 	(flatten-seq () () () () () trms)
       (let ((sequents (split1 ha hn ca cn)))
-	(format t "~%~a Sequences" (length sequents))
-        (assert (every #'sequent? sequents))
+	(display-sequents sequents)
+        #+dbg(assert (every #'sequent? sequents))
 	(filter-substs
 	 (gensubsts-sequents sequents))))))
 
+(defun display-sequents (sequents)
+  (pvs::error-format-if "~%Splits: ~a " (length sequents)))
+
 (defun filter-substs (score-substs)
   (remove-if #'(lambda (score-subst)
-		       (incomplete? (subst-of score-subst)))
+		       (incomplete? (substitution-of score-subst)))
 	  score-substs))
 	  
 (defun incomplete? (subst)
-   (some #'(lambda (x)
-	     (or (not (member x (dom subst)))
-		 (some #'(lambda (trm)
-			   (and trm (occurs-p x trm)))
-		       (mapcar #'cdr subst))))
-	 *lvars*))
+  (declare (special *lvars*))
+  (some #'(lambda (x)
+	    (or (not (member x (dom subst)))
+		(some #'(lambda (trm)
+			  (and trm (occurs-p x trm)))
+		      (mapcar #'cdr subst))))
+	*lvars*))
 
 ;; Collect all possible substitutions for a given set of sequents
 
@@ -60,75 +64,80 @@
        (every #'node-p (second seq))))
 
 (defun gensubsts-sequents (sequents &optional (score-substs
-					       (list (empty-score-subst))))
-  (assert (every #'sequent? sequents))
+					       (list *empty-score-subst*)))
+  #+dbg(assert (every #'sequent? sequents))
   (if (null sequents) score-substs
-      (gensubsts-sequents (cdr sequents)
-			  (destructuring-bind (hyps concs) (car sequents)
-			    (let ((*state* (init-dp-state
-					    (mapcar #'(lambda (trm)
-							(equality-p trm))
-					      hyps))))
-			      (declare (special *state*))
-			      (gensubsts-sequent hyps concs score-substs))))))
+      (let ((new-score-substs
+	     (destructuring-bind (hyps concs) (car sequents)
+	       (let ((*state* (init-dp-state
+			       (mapcar #'(lambda (trm)
+					   (equality-p trm))
+				 hyps))))
+		 (declare (special *state*))
+		 (score-substs-union score-substs
+				     (gensubsts-sequent hyps concs
+							(extended-score-substs score-substs)))))))
+	(gensubsts-sequents (cdr sequents) new-score-substs))))
+			 
+(defun extended-score-substs (score-substs)
+  (if (some #'(lambda (score-subst)
+		(null (substitution-of score-subst)))
+	    score-substs)
+      score-substs
+    (cons *empty-score-subst* score-substs)))
 
-(defun gensubsts-sequent (hyps concs score-substs &optional acc)
-  (assert (every #'node-p hyps))
-  (assert (every #'node-p concs))
-  (assert (every #'score-subst-p score-substs))
-  (format t "~2%Score Substs: ~a" score-substs)
-  (if (null score-substs) (nreverse acc)
-      (let* ((subst (subst-of (car score-substs)))
+(defun gensubsts-sequent (hyps concs score-substs)
+  #+dbg(assert (every #'node-p hyps))
+  #+dbg(assert (every #'node-p concs))
+  #+dbg(assert (every #'score-subst-p score-substs))
+  (if (null score-substs) nil
+      (let* ((subst (substitution-of (car score-substs)))
 	     (score (score-of (car score-substs)))
-	     (ext-score-substs (mapcar #'(lambda (subst)
-					   (make-score-subst :score (1+ score)
-							     :subst subst))
-				 (gensubsts-sequent1 hyps concs subst)))
-	     (new-score-substs (mapcar #'(lambda (subst)
-					  (make-score-subst :score 1
-							    :subst subst))
-				 (gensubsts-sequent1 hyps concs nil)))
-	     (newacc (remove-redundand-score-substs
-		       (cons (car score-substs) (append new-score-substs ext-score-substs)))))
-	(gensubsts-sequent hyps concs (cdr score-substs) newacc))))
+	     (new-score-substs
+	      (mapcar #'(lambda (subst)
+			  (make-score-subst :score (1+ score)
+					    :subst subst))
+		(gensubsts-sequent1 hyps concs subst))))
+	(score-substs-union new-score-substs
+			    (gensubsts-sequent hyps
+					       concs
+					       (cdr score-substs))))))
 
-(defun remove-redundand-score-substs (score-substs)
-  (add-score-substs score-substs nil))
+(defun score-substs-union (ss1 ss2)
+  (add-score-substs (append ss1 ss2) nil))
 
-(defun add-score-substs (score-substs1 score-substs2)
-  (if (null score-substs1) score-substs2
-      (add-score-substs (cdr score-substs1)
-			(add-score-subst (car score-substs1) score-substs2))))
+(defun add-score-substs (ss1 ss2)
+  (if (null ss1) ss2
+      (add-score-substs (cdr ss1)
+			(add-score-subst (car ss1) ss2))))
 
 (defun add-score-subst (score-subst score-substs)
-  (assert (score-subst-p score-subst))
-  (assert (every #'score-subst-p score-substs))
-  (multiple-value-bind (compatibles noncompatibles)
-      (pvs::partition score-substs #'(lambda (score-subst1)
-				       (subst= (subst-of score-subst)
-					       (subst-of score-subst1))))
-    (multiple-value-bind (worse notworse)
-	(pvs::partition compatibles #'(lambda (score-subst1)
-					(< (score-of score-subst1) (score-of score-subst))))
-      (if (null notworse)
-	  (cons score-subst noncompatibles)
-        score-substs))))
+  (if (null score-substs)
+      (list score-subst)
+    (let ((score-subst1 (car score-substs)))
+      (if (subst= (substitution-of score-subst1) (substitution-of score-subst))
+	  (if (< (score-of score-subst1) (score-of score-subst))
+	      (cons score-subst (cdr score-substs))
+	    score-substs)
+       (cons score-subst1 (add-score-subst score-subst (cdr score-substs)))))))
 
 (defun gensubsts-sequent1 (hyps concs subst)
   "Given a substitution list all substitutions generated from
    unifying complementary literals (involving variables)"
-  (assert (every #'node-p hyps))
-  (assert (every #'node-p concs))
-    (let ((*insts* nil))
-      (loop for hyp in hyps
-	    do (loop for conc in concs
-		     do (when (or (var-occurs-p hyp)
-				  (var-occurs-p conc)) 
-			  (let ((new-subst (E-unify hyp conc *state* subst))) 
-			      (setf *insts* (add-to-subst new-subst *insts*))))))
-      *insts*))
+  (declare (special *state*))
+  #+dbg(assert (every #'node-p hyps))
+  #+dbg(assert (every #'node-p concs))
+  (let ((*insts* nil))
+    (loop for hyp in hyps
+	  do (loop for conc in concs
+		   do (when (or (var-occurs-p hyp)
+				(var-occurs-p conc)) 
+			(let ((new-subst (E-unify hyp conc *state* subst))) 
+			  (setf *insts* (add-to-subst new-subst *insts*))))))
+    *insts*))
 
 (defun var-occurs-p (trm)
+  (declare (special *lvars*))
   (some #'(lambda (x)
 	    (occurs-p x trm))
 	*lvars*))
@@ -143,7 +152,7 @@
 ;; Splitting
 
 (defun split1 (ha hn ca cn)
-  (assert (every #'node-p (append ha hn ca cn)))
+  #+dbg(assert (every #'node-p (append ha hn ca cn)))
   (split1* ha hn ca cn))
 
 (defun split1* (ha hn ca cn)
@@ -169,9 +178,9 @@
 (defun split1- (ha e hn ca cn)
   (cond ((disjunction-p e)
 	 (multiple-value-bind (ha1 hn1 ca1 cn1) 
-	     (flatten-seq ha hn (list (args1 e)) ca cn ())
+	     (flatten-seq ha hn (list (lhs e)) ca cn ())
 	   (multiple-value-bind (ha2 hn2 ca2 cn2) 
-	       (flatten-seq ha hn (list (args2 e)) ca cn ())
+	       (flatten-seq ha hn (list (rhs e)) ca cn ())
 	     (append (split1* ha1 hn1 ca1 cn1)
 		     (split1* ha2 hn2 ca2 cn2)))))
 	((implication-p e)
@@ -184,16 +193,67 @@
 	(t
 	 (split1* (cons e ha) hn ca cn))))
 
+;; Propositional flattening and categorization into
+;; atomic and non-atomic literals hypotheses (ha hs) and conclusions (ca cs)
+
+(defun flatten-seq (ha hs hn ca cs cn)
+  #+dbg(assert (every #'node-p (append ha hs hn ca cs cn)))
+  (flatten-seq* ha hs hn ca cs cn))
+
+(defun flatten-seq* (ha hs hn ca cs cn)
+  (cond ((consp cn)
+	 (flatten-seq+ ha hs hn ca cs cn))
+	((consp hn)
+	 (flatten-seq- ha hs hn ca cs cn))
+	(t
+	 (values ha hs ca cs))))
+
+(defun flatten-seq+ (ha hs hn ca cs cn)
+  #+dbg(assert (consp cn))
+  (let* ((c (car cn))
+	 (cn (cdr cn)))
+    (cond ((negation-p c)
+	   (flatten-seq* ha hs (cons (arg 1 c) hn)
+			 ca cs cn))
+	  ((disjunction-p c)
+	   (flatten-seq* ha hs hn
+			 ca cs (cons (lhs c)
+				     (cons (rhs c) cn))))
+	  ((implication-p c)
+	   (flatten-seq* ha hs (cons (lhs c) hn)
+			 ca cs (cons (rhs c) cn)))
+	  ((conjunction-p c)
+	   (flatten-seq* ha hs hn
+			 ca (cons c cs) cn))
+	  (t
+	   (flatten-seq* ha hs hn
+			 (cons c ca) cs cn)))))
+
+(defun flatten-seq- (ha hs hn ca cs cn)
+  #+dbg(assert (consp hn))
+  (let* ((h (car hn))
+	 (hn (cdr hn)))
+    (cond ((negation-p h)
+	   (flatten-seq* ha hs hn
+			 ca cs (cons (arg 1 h) cn)))
+	  ((conjunction-p h)
+	   (flatten-seq* ha hs (cons (lhs h)
+				     (cons (rhs h) hn))
+			 ca cs cn))
+	  ((or (disjunction-p h)
+	       (implication-p h))
+	   (flatten-seq* ha (cons h hs) hn
+			 ca cs cn))
+	  (t
+	   (flatten-seq* (cons h ha) hs hn
+			 ca cs cn)))))
+
 (defun init-dp-state (&optional eqns)
   (let ((state (null-single-cong-state)))
     (loop for eqn in eqns
 	  do (when (equality-p eqn)
 	       (assert-eqn! (lhs eqn) (rhs eqn) state)))
     state))
-
-
-
-
 
 
 
