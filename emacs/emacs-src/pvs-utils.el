afa9b6595144128699f3dae745b974541c749817
owre <owre@ab98feae-9515-0410-836f-c1e8bd48b6aa>
1999-05-04 23:25:22 +0000

    Minor fix to short-file-name to ensure that ange-ftp does not kick in.
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@1132 ab98feae-9515-0410-836f-c1e8bd48b6aa

--- emacs/emacs-src/pvs-utils.el
+++ emacs/emacs-src/pvs-utils.el
@@ -581,7 +581,8 @@ The save-pvs-file command saves the PVS file of the current buffer."
 		((file-equal file lname)
 		 (setq shortname lname)))
 	  (setq lname (apply 'concat
-			(mapcar '(lambda (x) (concat "/" x)) dirnames)))
+			(cons "/" (mapcar '(lambda (x) (concat "/" x))
+				    dirnames))))
 	  (setq hname (concat "~" lname)))
 	shortname)
       file))
