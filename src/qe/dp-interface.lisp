4cb4a2ac1bc31b5a587257c32999a93ff8f21427
ruess <ruess@ab98feae-9515-0410-836f-c1e8bd48b6aa>
1999-05-10 22:16:02 +0000

    made the new-dp-state in protecting-dp-state special.
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@1194 ab98feae-9515-0410-836f-c1e8bd48b6aa

--- src/qe/dp-interface.lisp
+++ src/qe/dp-interface.lisp
@@ -11,38 +11,25 @@
 		     (dp::canon expr dp-state 'no-mod))
 		 dp-state expr))
 
+(defun dp-find (expr dp-state)
+  (apply-dp-func #'(lambda (expr dp-state)
+		     (dp::dp-find expr dp-state))
+		 dp-state expr))
+
 (defmacro protecting-dp-state (((new-dp-state old-dp-state)) &body body)
   (let ((resultsym (gensym)))
     `(let ((,new-dp-state (dp::push-new-cong-state ,old-dp-state))
 	   (,resultsym nil))
-       (setq ,resultsym
-	     (multiple-value-list (progn ,@body)))
-       (values-list ,resultsym))))
+       (declare (special ,new-dp-state))
+       (unwind-protect
+	   (progn
+	     (setq ,resultsym
+		   (multiple-value-list (progn ,@body)))
+	     (values-list ,resultsym))
+	 (dp::npop-cong-state ,new-dp-state)))))
 
-(defun solve-for (expr bndng state)
-  #+dbg(assert (not (negation? expr)))
-  #+dbg(assert (not (null state)))
-  #+dbg(assert (typep bndng 'bind-decl))
-  (let* ((trm (top-translate-to-dc expr))
-	 (canonized-trm (dp::canon trm state))
-	 (x (top-translate-to-dc (make-variable-expr bndng))))
-    (cond ((dp::false-p canonized-trm)
-	   (throw 'unsatisfiable nil))
-	  ((dp::true-p canonized-trm)
-	   *true*)
-	  (t
-	   (if (and (dp::occurs-p x trm)
-		    (not (dp::occurs-in-scope-of-uninterp-p x trm)))
-	       (let ((solved-trm (dp::normineq canonized-trm state x)))
-		 (unless (dp::well-formed-node-p solved-trm)
-		   (error-format-if "Term ~a may contain nonlinearity"
-				    (translate-from-dc canonized-trm))
-		   (throw 'unable nil))
-		 (translate-from-dc solved-trm))
-	       expr)))))
 
-(defun substitute-and-simplify (arg subst state)
-    (dp-canon (substit arg subst) state))
+
 
 
 
