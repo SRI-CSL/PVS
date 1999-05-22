d1e6af75430ff2460c19c4e09a252ba5b833c777
owre <owre@ab98feae-9515-0410-836f-c1e8bd48b6aa>
1999-05-22 17:31:52 +0000

    Unidid a change to tc-match* (type-name type-name)
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@1319 ab98feae-9515-0410-836f-c1e8bd48b6aa

--- src/tc-unify.lisp
+++ src/tc-unify.lisp
@@ -219,8 +219,8 @@
 						     (resolution farg))))
 				       bindings))
 		    (tc-match-names arg farg bindings))
-		(or (and (formal-type-decl? (declaration farg))
-			 bindings)
+		(or ;;(and (formal-type-decl? (declaration farg))
+			;; bindings)
 		    (tc-match-names arg farg bindings)))))))
 
 ;(defun tc-match-other-formals (arg farg bindings)
