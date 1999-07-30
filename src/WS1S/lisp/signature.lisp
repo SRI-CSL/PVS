46f23362959cf5eeabd3dc3b61e0f253ff2c872e
ruess <ruess@ab98feae-9515-0410-836f-c1e8bd48b6aa>
1999-07-30 19:01:15 +0000

    Adjusted to the change that record_prelude_example is not the last theory in the prelude anymore.
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@1800 ab98feae-9515-0410-836f-c1e8bd48b6aa

--- src/WS1S/lisp/signature.lisp
+++ src/WS1S/lisp/signature.lisp
@@ -4,33 +4,33 @@
 
 ;; Signature
 
-(def-pvs-term fset-of-nats "finite_set[nat]" "record_prelude_example" :nt type-expr)
+(def-pvs-term fset-of-nats "finite_set[nat]" "strings" :nt type-expr)
 
-(def-pvs-term empty-fset-of-nats "emptyset[nat]" "record_prelude_example"
+(def-pvs-term empty-fset-of-nats "emptyset[nat]" "strings"
                                  :expected "finite_set[nat]")
 
-(def-pvs-term add-to-fset "add[nat]" "record_prelude_example"
+(def-pvs-term add-to-fset "add[nat]" "strings"
                           :expected "[nat, finite_set[nat] -> finite_set[nat]]")
 
 
 (def-pvs-term the1      "the[nat]" "naturalnumbers")
-(def-pvs-term the2      "the[finite_set[nat]]" "record_prelude_example")
+(def-pvs-term the2      "the[finite_set[nat]]" "Fairctlops")
 (def-pvs-term minus1 "-" "naturalnumbers" :expected "[nat, nat -> nat]")
 (def-pvs-term plus1  "+" "naturalnumbers" :expected "[nat, nat -> nat]")
 
-(def-pvs-term add-operator "add" "record_prelude_example"
+(def-pvs-term add-operator "add" "strings"
                            :expected "[nat, set[nat] -> set[nat]]")
-(def-pvs-term remove-operator "remove" "record_prelude_example"
+(def-pvs-term remove-operator "remove" "strings"
                            :expected "[nat, set[nat] -> set[nat]]")
-(def-pvs-term singleton-operator "singleton" "record_prelude_example"
+(def-pvs-term singleton-operator "singleton" "strings"
                            :expected "[nat -> set[nat]]")
-(def-pvs-term union-operator    "union" "record_prelude_example"
+(def-pvs-term union-operator    "union" "strings"
                                     :expected "[set[nat], set[nat] -> set[nat]]")
-(def-pvs-term intersection-operator "intersection" "record_prelude_example"
+(def-pvs-term intersection-operator "intersection" "strings"
                                     :expected "[set[nat], set[nat] -> set[nat]]")
-(def-pvs-term set-difference-operator "difference" "record_prelude_example"
+(def-pvs-term set-difference-operator "difference" "strings"
                                     :expected "[set[nat], set[nat] -> set[nat]]")
-(def-pvs-term emptyset-operator "emptyset" "record_prelude_example"
+(def-pvs-term emptyset-operator "emptyset" "strings"
                                     :expected "set[nat]")
 
 (defun ws1s-types ()
