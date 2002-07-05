3517ed3211d32912b33436804d6c1f0f7235a641
ruess <ruess@ab98feae-9515-0410-836f-c1e8bd48b6aa>
2002-07-05 16:30:48 +0000

    added ics-state-unchanged?
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@3162 ab98feae-9515-0410-836f-c1e8bd48b6aa

--- src/ics-interface/ics-pvs.lisp
+++ src/ics-interface/ics-pvs.lisp
@@ -140,6 +140,12 @@
   (ics_process (state-unwrap state) (atom-unwrap atom)))
 
 
+(defun ics-state-unchanged? (state1 state2)
+   (assert (state-wrap? state1))
+   (assert (state-wrap? state2))
+   (zerop (ics_context_eq (state-unwrap state1) (state-unwrap2))))
+
+
 ;; Return a unique name for an expression
 
 (defvar *pvs-to-ics-symtab* 
