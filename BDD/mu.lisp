1c34f5b7c03d48d5791f87c1a4846faf8c4284b3
owre <owre@ab98feae-9515-0410-836f-c1e8bd48b6aa>
2002-10-23 10:55:53 +0000

    Changed from old ff:defforeign to ff:def-foreign-call forms
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@3536 ab98feae-9515-0410-836f-c1e8bd48b6aa

--- BDD/mu.lisp
+++ BDD/mu.lisp
@@ -18,77 +18,77 @@
 ;;;  Formula  ;;;
 ;;;;;;;;;;;;;;;;;
 
-(ff:defforeign 'mu_mk_false_formula :entry-point "mu___mu_mk_false_formula")
+(ff:def-foreign-call (mu_mk_false_formula "mu___mu_mk_false_formula") ())
 ;;; Formula mu_mk_false_formula (void)
-(ff:defforeign 'mu_mk_true_formula :entry-point "mu___mu_mk_true_formula")
+(ff:def-foreign-call (mu_mk_true_formula "mu___mu_mk_true_formula") ())
 ;;; Formula mu_mk_true_formula (void)
-(ff:defforeign 'mu_mk_bool_var :entry-point "mu___mu_mk_bool_var")
+(ff:def-foreign-call (mu_mk_bool_var "mu___mu_mk_bool_var") ())
 ;;; Formula mu_mk_bool_var (char *name)
-(ff:defforeign 'mu_check_bool_var :entry-point "mu___mu_check_bool_var")
+(ff:def-foreign-call (mu_check_bool_var "mu___mu_check_bool_var") ())
 ;;; Formula mu_check_bool_var
-(ff:defforeign 'mu_check_mk_bool_var :entry-point "mu___mu_check_mk_bool_var")
+(ff:def-foreign-call (mu_check_mk_bool_var "mu___mu_check_mk_bool_var") ())
 ;;; Formula mu_check_mk_bool_var
-(ff:defforeign 'mu_mk_ite_formula :entry-point "mu___mu_mk_ite_formula")
+(ff:def-foreign-call (mu_mk_ite_formula "mu___mu_mk_ite_formula") ())
 ;;; Formula mu_mk_ite_formula (Formula cond, Formula then_part, Formula else_part)
-(ff:defforeign 'mu_mk_curry_application :entry-point "mu___mu_mk_curry_application")
+(ff:def-foreign-call (mu_mk_curry_application "mu___mu_mk_curry_application") ())
 ;;; Formula mu_mk_curry_application (Term R, LIST subs, int curried)
-(ff:defforeign 'mu_mk_application :entry-point "mu___mu_mk_application")
+(ff:def-foreign-call (mu_mk_application "mu___mu_mk_application") ())
 ;;; Formula mu_mk_application (Term R, LIST subs, int curried)
-(ff:defforeign 'mu_mk_forall :entry-point "mu___mu_mk_forall") ;; (listvars fml1) always formula
-(ff:defforeign 'mu_mk_exists :entry-point "mu___mu_mk_exists") ;; (listvars fml1) always formula
+(ff:def-foreign-call (mu_mk_forall "mu___mu_mk_forall") ()) ;; (listvars fml1) always formula
+(ff:def-foreign-call (mu_mk_exists "mu___mu_mk_exists") ()) ;; (listvars fml1) always formula
 
-(ff:defforeign 'mu_mk_implies_formula :entry-point "mu___mu_mk_implies_formula") ;; (fml1 fml2)
-(ff:defforeign 'mu_mk_equiv_formula :entry-point "mu___mu_mk_equiv_formula");; (fml1 fml2)
-(ff:defforeign 'mu_mk_xor_formula :entry-point "mu___mu_mk_xor_formula");; (fml1 fml2)
-(ff:defforeign 'mu_mk_or_formula :entry-point "mu___mu_mk_or_formula");; (fml1 fml2)
-(ff:defforeign 'mu_mk_and_formula :entry-point "mu___mu_mk_and_formula");; (fml1 fml2)
-(ff:defforeign 'mu_mk_not_formula :entry-point "mu___mu_mk_not_formula");; (fml1 fml2)
-(ff:defforeign 'mu_mk_cofactor :entry-point "mu___mu_mk_cofactor");; (fml1 fml2) always formula
+(ff:def-foreign-call (mu_mk_implies_formula "mu___mu_mk_implies_formula") ()) ;; (fml1 fml2)
+(ff:def-foreign-call (mu_mk_equiv_formula "mu___mu_mk_equiv_formula") ());; (fml1 fml2)
+(ff:def-foreign-call (mu_mk_xor_formula "mu___mu_mk_xor_formula") ());; (fml1 fml2)
+(ff:def-foreign-call (mu_mk_or_formula "mu___mu_mk_or_formula") ());; (fml1 fml2)
+(ff:def-foreign-call (mu_mk_and_formula "mu___mu_mk_and_formula") ());; (fml1 fml2)
+(ff:def-foreign-call (mu_mk_not_formula "mu___mu_mk_not_formula") ());; (fml1 fml2)
+(ff:def-foreign-call (mu_mk_cofactor "mu___mu_mk_cofactor") ());; (fml1 fml2) always formula
 
 ;;;;;;;;;;;;;;;
 ;;;  Term   ;;;
 ;;;;;;;;;;;;;;;
-(ff:defforeign 'mu_mk_abstraction :entry-point "mu___mu_mk_abstraction")
+(ff:def-foreign-call (mu_mk_abstraction "mu___mu_mk_abstraction") ())
 ;;; Term mu_mk_abstraction (LIST vars, Formula f1)
-(ff:defforeign 'mu_mk_l_fixed_point :entry-point "mu___mu_mk_l_fixed_point")
+(ff:def-foreign-call (mu_mk_l_fixed_point "mu___mu_mk_l_fixed_point") ())
 ;;; Term mu_mk_fixed_point 
-(ff:defforeign 'mu_mk_g_fixed_point :entry-point "mu___mu_mk_g_fixed_point")
+(ff:def-foreign-call (mu_mk_g_fixed_point "mu___mu_mk_g_fixed_point") ())
 ;;; Term mu_mk_g_fixed_point 
-(ff:defforeign 'mu_mk_reach :entry-point "mu___mu_mk_reach")
+(ff:def-foreign-call (mu_mk_reach "mu___mu_mk_reach") ())
 ;;; Term mu_mk_reach (Term Next, Term S0, Term Inv)
-(ff:defforeign 'mu_mk_rel_var_dcl :entry-point "mu___mu_mk_rel_var_dcl")
+(ff:def-foreign-call (mu_mk_rel_var_dcl "mu___mu_mk_rel_var_dcl") ())
 ;;; Term mu_mk_rel_var_dcl (char *name) 
-(ff:defforeign 'mu_mk_rel_var_ :entry-point "mu___mu_mk_rel_var_")
+(ff:def-foreign-call (mu_mk_rel_var_ "mu___mu_mk_rel_var_") ())
 ;;; Term  mu_mk_rel_var_ (R_Interpret Ip, char *name)
-(ff:defforeign 'mu_mk_true_term :entry-point "mu___mu_mk_true_term")
+(ff:def-foreign-call (mu_mk_true_term "mu___mu_mk_true_term") ())
 ;;; Term  mu_mk_true_term (void)
-(ff:defforeign 'mu_mk_false_term :entry-point "mu___mu_mk_false_term")
+(ff:def-foreign-call (mu_mk_false_term "mu___mu_mk_false_term") ())
 ;;; Term  mu_mk_false_term (void)
-(ff:defforeign 'mu_mk_not_term :entry-point "mu___mu_mk_not_term") ;; (fml1)
-(ff:defforeign 'mu_mk_and_term :entry-point "mu___mu_mk_and_term") ;; (fml1 fml2)
-(ff:defforeign 'mu_mk_or_term :entry-point "mu___mu_mk_or_term") ;; (fml1 fml2)
-(ff:defforeign 'mu_mk_equiv_term :entry-point "mu___mu_mk_equiv_term") ;; (fml1 fml2)
-(ff:defforeign 'mu_mk_implies_term :entry-point "mu___mu_mk_implies_term") ;; (fml1 fml2)
-(ff:defforeign 'mu_mk_xor_term :entry-point "mu___mu_mk_xor_term") ;; (fml1 fml2)
+(ff:def-foreign-call (mu_mk_not_term "mu___mu_mk_not_term") ()) ;; (fml1)
+(ff:def-foreign-call (mu_mk_and_term "mu___mu_mk_and_term") ()) ;; (fml1 fml2)
+(ff:def-foreign-call (mu_mk_or_term "mu___mu_mk_or_term") ()) ;; (fml1 fml2)
+(ff:def-foreign-call (mu_mk_equiv_term "mu___mu_mk_equiv_term") ()) ;; (fml1 fml2)
+(ff:def-foreign-call (mu_mk_implies_term "mu___mu_mk_implies_term") ()) ;; (fml1 fml2)
+(ff:def-foreign-call (mu_mk_xor_term "mu___mu_mk_xor_term") ()) ;; (fml1 fml2)
 
-(ff:defforeign 'get_bdd_var_id :entry-point "mu___get_bdd_var_id") ;; (int)
-(ff:defforeign 'get_mu_bool_var_name :entry-point "mu___get_mu_bool_var_name") ;; (char)
+(ff:def-foreign-call (get_bdd_var_id "mu___get_bdd_var_id") ()) ;; (int)
+(ff:def-foreign-call (get_mu_bool_var_name "mu___get_mu_bool_var_name") ()) ;; (char)
 ;;;;;;;;;;;;;;;;;;;
 ;;;  Lists      ;;;
 ;;;;;;;;;;;;;;;;;;;
 ;; 
 
-(ff:defforeign 'append_cont :entry-point "mu___append_cont")
-(ff:defforeign 'empty_list :entry-point "mu___empty_list")
+(ff:def-foreign-call (append_cont "mu___append_cont") ())
+(ff:def-foreign-call (empty_list "mu___empty_list") ())
 
 ;;;
 ;;; Flags
 
-(ff:defforeign 'set_mu_bdd_ordering :entry-point "mu___set_mu_bdd_ordering")
-(ff:defforeign 'set_mu_warnings :entry-point "mu___set_mu_warnings")
-(ff:defforeign 'set_mu_simplify_frontier :entry-point "mu___set_mu_simplify_frontier")
-(ff:defforeign 'set_mu_verbose :entry-point "mu___set_mu_verbose")
-(ff:defforeign 'set_mu_bdd_use_neg_edges :entry-point "mu___set_mu_bdd_use_neg_edges")
+(ff:def-foreign-call (set_mu_bdd_ordering "mu___set_mu_bdd_ordering") ())
+(ff:def-foreign-call (set_mu_warnings "mu___set_mu_warnings") ())
+(ff:def-foreign-call (set_mu_simplify_frontier "mu___set_mu_simplify_frontier") ())
+(ff:def-foreign-call (set_mu_verbose "mu___set_mu_verbose") ())
+(ff:def-foreign-call (set_mu_bdd_use_neg_edges "mu___set_mu_bdd_use_neg_edges") ())
 
 ;;
 ;;
@@ -99,16 +99,16 @@
 ;;;  print      ;;;
 ;;;;;;;;;;;;;;;;;;;
 
-(ff:defforeign 'pvs_mu_print_formula :entry-point "mu___pvs_mu_print_formula")
-(ff:defforeign 'pvs_mu_print_term :entry-point "mu___pvs_mu_print_term")
+(ff:def-foreign-call (pvs_mu_print_formula "mu___pvs_mu_print_formula") ())
+(ff:def-foreign-call (pvs_mu_print_term "mu___pvs_mu_print_term") ())
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Main function   ;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(ff:defforeign 'mu_init :entry-point "mu___mu_init")
-(ff:defforeign 'mu_quit :entry-point "mu___mu_quit")
-(ff:defforeign 'modelcheck_formula :entry-point "mu___modelcheck_formula")
+(ff:def-foreign-call (mu_init "mu___mu_init") ())
+(ff:def-foreign-call (mu_quit "mu___mu_quit") ())
+(ff:def-foreign-call (modelcheck_formula "mu___modelcheck_formula") ())
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Global variables     ;;;
@@ -1446,19 +1446,19 @@ time).  Verbose? set to T provides more information."
 	    (mu-make-bool-var bvarname)))))
 
 (defun mu-mk-rel-var (bvarname)
-(mu_mk_rel_var_ (ff:string-to-char* bvarname)))
+  (mu_mk_rel_var_ (ff:string-to-char* bvarname)))
 
 (defun mu-mk-rel-var-dcl (bvarname)
-(mu_mk_rel_var_dcl (ff:string-to-char* bvarname)))
+  (mu_mk_rel_var_dcl (ff:string-to-char* bvarname)))
 
 (defun mu-check-bool-var (bvarname)
-(mu_check_bool_var (ff:string-to-char* bvarname)))
+  (mu_check_bool_var (ff:string-to-char* bvarname)))
 
 (defun mu-make-bool-var (bvarname)
- (if *build-mu-term*
-  (mu_mk_rel_var_ (ff:string-to-char* bvarname))
-  (mu_check_mk_bool_var (ff:string-to-char* bvarname))
-))
+  (if *build-mu-term*
+      (mu_mk_rel_var_ (ff:string-to-char* bvarname))
+      (mu_check_mk_bool_var (ff:string-to-char* bvarname))
+      ))
 
 ;;
 ;;
