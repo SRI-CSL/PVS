f92bfdc6cffe6f6e94af1fa94ef4853a25c9f544
owre <owre@ab98feae-9515-0410-836f-c1e8bd48b6aa>
2002-12-10 04:17:29 +0000

    Fixed to optimize def-foreign-calls
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@3695 ab98feae-9515-0410-836f-c1e8bd48b6aa

--- src/utils/file-utils.lisp
+++ src/utils/file-utils.lisp
@@ -1,8 +1,10 @@
 (in-package :pvs)
-(require :foreign)
+(eval-when (compile)
+  (require :foreign))
 (export '(file-exists-p directory-p read-permission? write-permission?
 			file-write-time get-file-info))
 
+(eval-when (eval compile load)
 (ff:def-foreign-call fileutils___file_exists_p
     ((filename (* :char) simple-string))
   #+(version>= 6) :strings-convert #+(version>= 6) nil
@@ -45,6 +47,7 @@
   :arg-checking nil
   :call-direct t
   :returning :int)
+)
 
 ;;;
 
@@ -54,45 +57,37 @@
 (defun file-exists-p (filename)
   (let ((exp-file (expanded-tilde-namestring filename)))
     (and exp-file
-	 (excl:with-native-string
-	  (cstr exp-file)
-	  (zerop (fileutils___file_exists_p cstr))))))
+	 (zerop (fileutils___file_exists_p exp-file)))))
 
 (defun directory-p (filename)
   (when filename
     (let ((filestring (expanded-tilde-namestring filename)))
-      (excl:with-native-string
-       (cstr filestring)
-       (unless (zerop (fileutils___directory_p cstr))
-	 (pathname (if (char= (char filestring (1- (length filestring))) #\/)
-		       filestring
-		       (concatenate 'string filestring "/"))))))))
+      (unless (zerop (fileutils___directory_p filestring))
+	(pathname (if (char= (char filestring (1- (length filestring))) #\/)
+		      filestring
+		      (concatenate 'string filestring "/")))))))
 
 (defun read-permission? (filename)
-  (excl:with-native-string
-   (cstr (expanded-tilde-namestring filename))
-   (zerop (fileutils___read_permission_p cstr))))
+  (zerop (fileutils___read_permission_p
+	  (expanded-tilde-namestring filename))))
 
 (defun write-permission? (filename)
-  (excl:with-native-string
-   (cstr (expanded-tilde-namestring filename))
-   (zerop (fileutils___write_permission_p cstr))))
+  (zerop (fileutils___write_permission_p
+	  (expanded-tilde-namestring filename))))
 
 (defconstant u1970 (encode-universal-time 0 0 0 1 1 1970 0))
 
 (defun file-write-time (filename)
-  (excl:with-native-string
-   (cstr (expanded-tilde-namestring filename))
-   (let ((date (fileutils___file_write_time cstr)))
-     (unless (zerop date)
-       (+ date u1970)))))
+  (let ((date (fileutils___file_write_time
+	       (expanded-tilde-namestring filename))))
+    (unless (zerop date)
+      (+ date u1970))))
 
 ;;; #(dev inode mtime isdir mode)
 (let ((fstat-array
        (make-array 2 :initial-element 0 :element-type 'fixnum)))
   (defun get-file-info (filename)
-    (excl:with-native-string
-     (cstr (expanded-tilde-namestring filename))
-     (when (zerop (fileutils___getfileinfo cstr fstat-array))
-       (list (aref fstat-array 0)
-	     (aref fstat-array 1))))))
+    (when (zerop (fileutils___getfileinfo
+		  (expanded-tilde-namestring filename) fstat-array))
+      (list (aref fstat-array 0)
+	    (aref fstat-array 1)))))
