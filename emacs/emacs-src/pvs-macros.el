4f8d8ae49d29bfabe680875da5ba9c47cd40cb64
owre <owre@ab98feae-9515-0410-836f-c1e8bd48b6aa>
2004-10-05 00:53:12 +0000

    Added better with-timeout macro (from Jerry James)
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@4245 ab98feae-9515-0410-836f-c1e8bd48b6aa

--- emacs/emacs-src/pvs-macros.el
+++ emacs/emacs-src/pvs-macros.el
@@ -26,6 +26,38 @@ define NAME as a PVS command."
 	(list 'put (list 'quote name) ''pvs-command (list 'quote class))
 	(cons 'defun (cons name (cons arglist (cons docstring body))))))
 
+
+;; This is courtesy of Jerry James.
+(when (string-match "XEmacs" (emacs-version))
+    
+(defun with-timeout-handler (tag)
+  (throw tag 'timeout))
+
+(defmacro with-timeout (list &rest body)
+  "Run BODY, but if it doesn't finish in SECONDS seconds, give up.
+If we give up, we run the TIMEOUT-FORMS and return the value of the last one.
+The call should look like:
+ (with-timeout (SECONDS TIMEOUT-FORMS...) BODY...)
+The timeout is checked whenever Emacs waits for some kind of external
+event \(such as keyboard input, input from subprocesses, or a certain time);
+if the program loops without waiting in any way, the timeout will not
+be detected."
+  (let ((seconds (car list))
+	(timeout-forms (cdr list)))
+    `(let ((with-timeout-tag (cons nil nil))
+	   with-timeout-value with-timeout-timer)
+      (if (catch with-timeout-tag
+	    (progn
+	      (setq with-timeout-timer
+		    (start-itimer "internal-itimer" #'with-timeout-handler
+				  ,seconds nil t t with-timeout-tag))
+	      (setq with-timeout-value (progn ,@body))
+	      nil))
+	  (progn ,@timeout-forms)
+	(delete-itimer with-timeout-timer)
+	with-timeout-value))))
+)
+
 (provide 'pvs-macros)
 
 
