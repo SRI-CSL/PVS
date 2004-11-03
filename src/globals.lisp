8aa28739530808bb6beae08ce719bcc7518dadb6
owre <owre@ab98feae-9515-0410-836f-c1e8bd48b6aa>
2004-11-03 22:46:50 +0000

    Remove -beta from version
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@4312 ab98feae-9515-0410-836f-c1e8bd48b6aa

--- src/globals.lisp
+++ src/globals.lisp
@@ -52,7 +52,7 @@
     (subseq excl::cl-release-date 0 end)))
 
 (eval-when (eval compile load)
-  (defparameter *pvs-version* "3.2-beta")
+  (defparameter *pvs-version* "3.2")
 
   ;; Not used in PVS sources, but may be useful for patches, strategies, etc.
   ;;(pushnew (intern (format nil "pvs~a" *pvs-version*) :keyword) *features*)
