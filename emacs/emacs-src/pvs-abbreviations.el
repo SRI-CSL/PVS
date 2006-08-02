9af7d30c9df040e96355fc230c3d8a45bad3c7f3
owre <owre@ab98feae-9515-0410-836f-c1e8bd48b6aa>
2006-08-02 11:05:52 +0000

    Added new copyright
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@4759 ab98feae-9515-0410-836f-c1e8bd48b6aa

--- emacs/emacs-src/pvs-abbreviations.el
+++ emacs/emacs-src/pvs-abbreviations.el
@@ -11,6 +11,25 @@
 ;; 12-Jul-1994		Sam Owre	
 ;;    Removed key-bindings for C-z C-g and C-h p
 
+;; --------------------------------------------------------------------
+;; PVS
+;; Copyright (C) 2006, SRI International.  All Rights Reserved.
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 2
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+;; --------------------------------------------------------------------
+
 (global-set-key "\C-x\C-z" 'suspend-pvs)
 (global-set-key "\C-x\C-c" 'exit-pvs)
 (global-set-key "\C-c\C-f" 'find-pvs-file)
