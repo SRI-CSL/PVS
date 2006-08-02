54a4de1538f0fa64b8f7637fe68a6367c2c70a1e
owre <owre@ab98feae-9515-0410-836f-c1e8bd48b6aa>
2006-08-02 21:19:15 +0000

    Added new copyright
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@4762 ab98feae-9515-0410-836f-c1e8bd48b6aa

--- emacs/emacs-src/pvs-prover.el
+++ emacs/emacs-src/pvs-prover.el
@@ -5,10 +5,27 @@
 ;; Last Modified By: Sam Owre
 ;; Last Modified On: Sun Oct 15 02:33:07 1995
 ;; Update Count    : 21
-;; Status          : Beta test
-;; 
-;; HISTORY
+;; Status          : Stable
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; --------------------------------------------------------------------
+;; PVS
+;; Copyright (C) 2006, SRI International.  All Rights Reserved.
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 2
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+;; --------------------------------------------------------------------
 
 ;; prove	        - prove-file-at
 ;; redo-proof           - prove-file-at
