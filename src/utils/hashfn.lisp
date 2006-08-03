e85ce9b3dfa679d0da4919653bb0ed4294e673c5
owre <owre@ab98feae-9515-0410-836f-c1e8bd48b6aa>
2006-08-03 07:03:34 +0000

    Added new copyright
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@4775 ab98feae-9515-0410-836f-c1e8bd48b6aa

--- src/utils/hashfn.lisp
+++ src/utils/hashfn.lisp
@@ -1,4 +1,3 @@
-(in-package 'pvs)
 
 ;;; FILE: hashfn.lisp
 ;;; AUTHOR: David Cyrluk
@@ -37,6 +36,27 @@
 
 ;;; Sam also suggests adding type declarations saying that the
 ;;; arguments to plus, etc are integers.
+
+;; --------------------------------------------------------------------
+;; PVS
+;; Copyright (C) 2006, SRI International.  All Rights Reserved.
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 2
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+;; --------------------------------------------------------------------
+
+(in-package 'pvs)
 
 ;(declaim (function pvs-sxhash* (T list) (integer 0 65535)))
 
