2a77a1f540ac5482b0fa413f199b36f0733cf7f4
owre <owre@ab98feae-9515-0410-836f-c1e8bd48b6aa>
2006-08-03 04:50:23 +0000

    Added new copyright
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@4769 ab98feae-9515-0410-836f-c1e8bd48b6aa

--- src/tclib.lisp
+++ src/tclib.lisp
@@ -7,7 +7,25 @@
 ;; Update Count    : 49
 ;; Status          : Stable
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;   Copyright (c) 2002 SRI International, Menlo Park, CA 94025, USA.
+
+;; --------------------------------------------------------------------
+;; PVS
+;; Copyright (C) 2006, SRI International.  All Rights Reserved.
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 2
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+;; --------------------------------------------------------------------
 
 (in-package :pvs)
 
