582ebbba66ce65fb98e99203e69c6e24c5429ad9
owre <owre@ab98feae-9515-0410-836f-c1e8bd48b6aa>
2009-02-10 21:38:52 +0000

    Minor change
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@5368 ab98feae-9515-0410-836f-c1e8bd48b6aa

--- src/abstraction/abstract.lisp
+++ src/abstraction/abstract.lisp
@@ -27,7 +27,7 @@
 ;; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 ;; --------------------------------------------------------------------
 
-(in-package 'pvs)
+(in-package :pvs)
 
 
 (defvar *abs-cache+* nil)
