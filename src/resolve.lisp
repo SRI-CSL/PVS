b62742731b177a35706f8f0ebcf801ab224deae9
owre <owre@ab98feae-9515-0410-836f-c1e8bd48b6aa>
2011-09-28 21:41:15 +0000

    Modified typecheck* (actual) to remove other resolutions when the name-expr resolution is set.
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@5887 ab98feae-9515-0410-836f-c1e8bd48b6aa

--- src/resolve.lisp
+++ src/resolve.lisp
@@ -691,7 +691,9 @@
 				  (null (place (type (car tres)))))
 			 (setf (place (type (car tres))) (place (expr act))))
 		       (setf (type-value act) (type (car tres)))
-		       (push 'type (types (expr act)))))))))))
+		       (push 'type (types (expr act)))
+		       (when (cdr (resolutions (expr act)))
+			 (setf (resolutions (expr act)) tres))))))))))
       ;; with-no-type-errors not needed here;
       ;; the expr typechecks iff the subtype does.
       (set-expr (typecheck* (expr act) nil nil nil)
