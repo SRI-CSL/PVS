7b5a84e7fe9a32b24c6ba7b9331e37cc4e4b5397
saadati <saadati@ab98feae-9515-0410-836f-c1e8bd48b6aa>
2012-03-02 20:58:23 +0000

    nomsg
    git-svn-id: svn+ssh://spartan.csl.sri.com/svn/public/pvs/trunk@6000 ab98feae-9515-0410-836f-c1e8bd48b6aa

---
+++ eclipse/plugin/src/com/sri/csl/pvs/declarations/PVSDeclarationPlace.java
@@ -0,0 +1,13 @@
+package com.sri.csl.pvs.declarations;
+
+public class PVSDeclarationPlace {
+	public int x1, y1, x2, y2;
+	
+	public PVSDeclarationPlace(int x1, int y1, int x2, int y2) {
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;		
+	}
+
+}
