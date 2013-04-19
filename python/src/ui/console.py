59b2c6a3a89de2e98bb4e2778fe14ddae04cb93c
happyhuman <saadati@ai.sri.com>
2013-04-19 13:45:55 -0700

    Cleaned up the console code a little.

--- python/src/ui/console.py
+++ python/src/ui/console.py
@@ -5,8 +5,7 @@ import wx
 import codecs
 import util
 from promptprocessor import isPrompt
-import constants
-from constants import NEWLINE, PVS_MODE, EMPTY_STRING, PVS_MODE_OFF, PVS_MODE_UNKNOWN
+from constants import NEWLINE, PVS_MODE, EMPTY_STRING, PVS_MODE_OFF, PVS_MODE_UNKNOWN, LABEL_PVS_CONSOLE
 
 log = util.getLogger(__name__)
 
@@ -15,43 +14,37 @@ class PVSConsole(wx.Frame):
     This console shows the PVS output, and the user can enter a line of input"""
     
     def __init__(self):
+        """Initializes the console GUI"""
         wx.Frame.__init__(self, wx.GetApp().TopWindow, title=constants.LABEL_PVS_CONSOLE)
         self.prompt = EMPTY_STRING
         self.history = []
         self.pvsout = wx.TextCtrl(self, wx.ID_ANY, constants.EMPTY_STRING, style=wx.TE_MULTILINE | wx.TE_READONLY)
         self.pvsin = wx.TextCtrl(self, wx.ID_ANY, constants.EMPTY_STRING, style=wx.TE_PROCESS_ENTER)
         
-        sizer_10 = wx.BoxSizer(wx.VERTICAL)
-        #sizer_7.Add(util.console, 1, wx.EXPAND, 0)
-        # sizer_7 = wx.BoxSizer(wx.VERTICAL)
-        # sizer_7.Add(self, 1, wx.EXPAND, 0)
-        # self.SetSizer(sizer_7)
-        ###sizer_9.Add(self.pvseditor, 1, wx.EXPAND, 0)
-        sizer_10.Add(self.pvsout, 1, wx.EXPAND, 0)
-        sizer_10.Add(self.pvsin, 0, wx.EXPAND, 0)       
-        self.SetSizer(sizer_10)
+        consoleSizer = wx.BoxSizer(wx.VERTICAL)
+        consoleSizer.Add(self.pvsout, 1, wx.EXPAND, 0)
+        consoleSizer.Add(self.pvsin, 0, wx.EXPAND, 0)       
+        self.SetSizer(consoleSizer)
         self.SetSize((700, 200))
         currentPosition = self.GetPositionTuple()
         self.SetPosition((currentPosition[0], currentPosition[1] + 425))
-        self.setBidnings()
-
-    def setBidnings(self):
         self.Bind(wx.EVT_TEXT_ENTER, self.onPVSInTextEntered, self.pvsin)
         self.Bind(wx.EVT_TEXT, self.onPVSInText, self.pvsin)
 
     def clearOut(self):
+        """Clears the PVS output text in the console"""
         self.pvsout.Clear()
         log.debug("Clearing pvsout")
         
     def clearIn(self):
+        """Clears the PVS prompt box in the console"""
         self.pvsin.Clear()
         log.debug("Clearing pvsin")
         
     def initializeConsole(self):
-        #wx.MutexGuiEnter()
+        """Initializes PVS In and Out"""
         self.pvsout.Clear()
         self.pvsin.Clear()
-        #wx.MutexGuiLeave()
         self.prompt = EMPTY_STRING
         self.history = []
         util.frame.updateFrame(PVS_MODE_OFF)
@@ -80,7 +73,8 @@ class PVSConsole(wx.Frame):
         self.writeTextToIn(prompt)
         self.prompt = prompt
 
-    def onPVSInTextEntered(self, event):  # wxGlade: PVSMainFrame.<event_handler>
+    def onPVSInTextEntered(self, event):
+        """This method is called whenever the user types something in the prompt and Enters."""
         log.info("Event handler `onPVSConsoleTextEntered' not implemented")
         whole = event.GetString()
         pl = len(self.prompt)
@@ -93,7 +87,8 @@ class PVSConsole(wx.Frame):
             util.runner.tellPVS(command + NEWLINE)
         #event.Skip()
 
-    def onPVSInText(self, event):  # wxGlade: PVSMainFrame.<event_handler>
+    def onPVSInText(self, event):
+        """This method is called whenever PVS sends some text to the Editor"""
         st = event.GetString()
         log.info("Received: %s", st)
         if not st.startswith(self.prompt):
