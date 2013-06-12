bc1b708b2ba5208b3fad44999f07b59f3f2a2f90
Sam Owre <owre@csl.sri.com>
2013-06-12 10:48:10 -0700

    Cleanup

--- src/interface/xmlrpc_test.py
+++ src/interface/xmlrpc_test.py
@@ -30,7 +30,6 @@ class XMLRpcError(Error):
 
 class MethodError(Error):
     """Exception raised for errors in executing the method."""
-
     def __init__(self, request):
         self.request = request
 
@@ -41,7 +40,7 @@ class RequestHandler(SimpleXMLRPCRequestHandler):
 
 class PVS_XMLRPC(object):
 
-    def __init__(self, host='localhost', port=55224):
+    def __init__(self, host='localhost', port=22335):
         # Create server
         print 'Initializing gui_server with ({0}, {1})'.format(host,port)
         self.ctr = 0
@@ -51,27 +50,38 @@ class PVS_XMLRPC(object):
         self.gui_server.register_function(self.request, 'request')
         self.server_thread = threading.Thread(target=self.gui_server.serve_forever)
         self.server_thread.start()
-        self.pvs_proxy = xmlrpclib.ServerProxy('http://localhost:55223')
+        self.pvs_proxy = xmlrpclib.ServerProxy('http://localhost:22334')
         self.json_methods = {'debug': self.pvs_debug,
                              'info': self.pvs_info,
                              'warning': self.pvs_warning}
 
+    def pvs_request(self, method, params):
+        """ Send a request to PVS """
+        reqid = 'gui_{0}'.format(self.ctr)
+        self.ctr += 1
+        request = {'method': method, 'params': params, 'id': reqid}
+        self.pvs_proxy.pvs.request(json.dumps(request), self.gui_url)
+
     def request(self, json_string):
         """
-        An XML-RPC request presumably from PVS - typically a JSON-RPC message
-        without id.  This always returns a value.  If the JSON-RPC message
-        cannot be parsed, or some other problem happens at the XML-RPC level,
-        then it returns an error string.  If the JSON-RPC message is parsed,
-        but doesn't include an id, then if an error occurs it is returned as a
-        string.  If no id and no error, "null" is returned.  Otherwise,
-        a valid JSON-RPC response/error form is returned.
+        Process a request from PVS
+        
+        An XML-RPC request presumably from PVS - typically a JSON-RPC
+        message without id.  This always returns a value at the XML-RPC
+        level.  If the JSON-RPC message cannot be parsed, or some other
+        problem happens at the XML-RPC level, then it returns an error
+        string.  If the JSON-RPC message is parsed, but doesn't include an
+        id, then if an error occurs it is returned as a string.  If no id
+        and no error, "null" is returned.  Otherwise, a valid JSON-RPC
+        response/error form is returned.
         """
         print('\nrequest = {0}\n'.format(json_string))
         try:
             request = json.loads(json_string, object_hook=self.request_check)
             return self.process_request(request)
         except TypeError as err:
-            return 'request: {0} is of type {1}, string expected'.format(json_string, type(json_string))
+            return 'request: {0} is of type {1}, string expected'.format(
+                json_string, type(json_string))
         except ValueError as err:
             return 'request: {0} is invalid - {1}'.format(json_string, err)
         except XMLRpcError as err:
@@ -79,33 +89,10 @@ class PVS_XMLRPC(object):
             # This is just an XML-RPC answer.
             return 'request: {0} is invalid - {1}'.format(json_string, err)
 
-    def pvs_request(self, method, params):
-        reqid = 'gui_{0}'.format(self.ctr)
-        self.ctr += 1
-        request = {'method': method, 'params': params, 'id': reqid}
-        self.pvs_proxy.pvs.request(json.dumps(request), self.gui_url)
-
-    def parse_error(self, msg):
-        # id is not available
-        return json.dumps({"Error": {"code": -32700, "message": msg}})
-
-    def request_check(self, dct):
-        """
-        Checks that the request is a JSON object (i.e., dictionary)
-        with a method, jsonrpc of '2.0', optional id and params, and
-        nothing else.
-        """
-        if not isinstance(dct, dict):
-            raise XMLRpcError(dct, 'Request must be a JSON object')
-        if 'jsonrpc' not in dct or dct['jsonrpc'] != '2.0':
-            raise XMLRpcError(dct, 'Request must include "jsonrpc": "2.0"')
-        if 'method' not in dct:
-            raise XMLRpcError(dct, 'Request must include a method')
-        if not all(k in ['jsonrpc', 'method', 'id', 'params'] for k in dct.keys()):
-            raise XMLRpcError(dct, 'Request must only have "jsonrpc", "method", "id", and "params" fields')
-        return dct
-
     def process_request(self, request):
+        """
+        Processes the json-rpc request
+        """
         method = request['method']
         if 'id' in request:
             id = request['id']
@@ -141,24 +128,34 @@ class PVS_XMLRPC(object):
             else:
                 return json.dumps({'id': id, 'error': errmsg})
 
+    def request_check(self, dct):
+        """
+        Checks that the request is a JSON object (i.e., dictionary)
+        with a method, jsonrpc of '2.0', optional id and params, and
+        nothing else.
+        """
+        if not isinstance(dct, dict):
+            raise XMLRpcError(dct, 'Request must be a JSON object')
+        if 'jsonrpc' not in dct or dct['jsonrpc'] != '2.0':
+            raise XMLRpcError(dct, 'Request must include "jsonrpc": "2.0"')
+        if 'method' not in dct:
+            raise XMLRpcError(dct, 'Request must include a method')
+        if not all(k in ['jsonrpc', 'method', 'id', 'params'] for k in dct.keys()):
+            raise XMLRpcError(dct, 'Request must only have "jsonrpc", "method", "id", and "params" fields')
+        return dct
+
     def pvs_debug(self, msg):
+        """ JSON-RPC method """
         print('DEBUG: {0}'.format(msg))
 
     def pvs_info(self, msg):
+        """ JSON-RPC method """
         print('INFO: {0}'.format(msg))
 
     def pvs_warning(self, msg):
+        """ JSON-RPC method """
         print('WARNING: {0}'.format(msg))
         
-
-def get_lan_ip_addr():
-    '''Gets the local IP address if connected through LAN'''
-    p = subprocess.Popen(["/sbin/ifconfig"], stdout=subprocess.PIPE)
-    ifc_resp = p.communicate()
-    patt = re.compile(r'inet\s*\w*\S*:\s*(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
-    resp = [ip for ip in patt.findall(ifc_resp[0]) if not ip.startswith("127.")][:1]
-    return resp[0]
-
 def main():
     ''' Starts the server, runs tests '''
     
