d01e57b0338289c4c35b74757de6ddbca5dd4513
Leonard <leonard.gerard@sri.com>
2014-02-13 21:14:38 -0800

    Fix macos dynlib and remove unnecessary symbol map
    Needs testing on all the previous platforms.

--- src/utils/file_utils.c
+++ src/utils/file_utils.c
@@ -24,37 +24,38 @@
 #include <unistd.h>
 #include <errno.h>
 
+
 static struct stat finfo;
 
 int
-file_exists_p(char *filename)
+fileutils___file_exists_p(char *filename)
 {
   return(access(filename, F_OK));
 }
 
 int
-directory_p(char *filename)
+fileutils___directory_p(char *filename)
 {
   if (stat(filename,&finfo) == -1) return(0);
   return(S_ISDIR(finfo.st_mode));
 }
 
 int
-read_permission_p(char *filename)
+fileutils___read_permission_p(char *filename)
 {
   if (access(filename, R_OK) == 0) return(0);
   return(errno);
 }  
 
 int
-write_permission_p(char *filename)
+fileutils___write_permission_p(char *filename)
 {
   if (access(filename, W_OK) == 0) return(0);
   return(errno);
 }
 
 long int
-file_write_time(char *filename)
+fileutils___file_write_time(char *filename)
 {
   if (!(stat(filename,&finfo))) {
     return((long) finfo.st_mtime);
@@ -63,7 +64,7 @@ file_write_time(char *filename)
 }
 
 int
-getfileinfo(char *filename, int *i)
+fileutils___getfileinfo(char *filename, int *i)
 {
   if (!(stat(filename,&finfo))) {
     i[0] = (int) finfo.st_dev;
