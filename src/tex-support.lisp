11424b19cdbd512c4e5e4211018f3d7dc48f8fbd
Sam Owre <owre@csl.sri.com>
2015-08-22 12:15:37 -0700

    Minor fix to latex-proof*

--- src/tex-support.lisp
+++ src/tex-support.lisp
@@ -455,7 +455,8 @@ useful if more than one specification is to be included in one document")
   (when (or (null terse?) (null flag))
     (latex-print proofstate stream))
   (when (current-rule proofstate)
-    (format stream "~%Rule? ~VT~a~%" *prover-indent* (current-rule proofstate)))
+    (format stream "~%Rule? ~VT~a~%"
+      *prover-indent* (latex-protect (format nil "~s" (current-rule proofstate)))))
   (latex-proof-printout (printout proofstate) stream)
   (when (null (done-subgoals proofstate))
     (format stream "~%This completes the proof of {\\tt ~a}.~%"
