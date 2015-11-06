3e1ac70ba7190450422fdcf2c92a7867f712a5f4
Paolo Masci <paolo.masci@gmail.com>
2015-11-06 15:43:48 +0000

    improved website

--- website/js/index.js
+++ website/js/index.js
@@ -1,7 +1,17 @@
+/* global document */
 function googlesearch(query) {
+    "use strict";
     query.q.value = "site:pvs.csl.sri.com " + document.getElementById("search_input").value;
 }
-
-function onClick(_this) {
-    _this.form.target = '_blank';
+function googlescholarsearch(query) {
+    "use strict";
+    query.q.value = document.getElementById("search_pvs").value + "&" + document.getElementById("search_date").value;
+}
+function onClick(el) {
+    "use strict";
+    el.form.target = '_blank';
+}
+function usr_click(id) {
+    "use strict";
+    document.getElementById(id).click();
 }
