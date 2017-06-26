77aedbaee53cdee6585b8f4e1f6bd630526a9278
Sam Owre <owre@csl.sri.com>
2017-06-26 00:53:54 -0700

    Modified everywhere-true? and everywhere-false for a faster test

--- src/datatype.lisp
+++ src/datatype.lisp
@@ -5379,14 +5379,16 @@ function, tuple, or record type")
   nil)
 
 (defmethod everywhere-true? ((expr lambda-expr))
-  (tc-eq (expression expr) *true*))
+  (and (name-expr? (expression expr))
+       (eq (declaration (expression expr)) (declaration *true*))))
 
 (defmethod everywhere-false? (expr)
   (declare (ignore expr))
   nil)
 
 (defmethod everywhere-false? ((expr lambda-expr))
-  (tc-eq (expression expr) *false*))
+  (and (name-expr? (expression expr))
+       (eq (declaration (expression expr)) (declaration *false*))))
 
 (defun mk-identity-fun (te)
   (let ((act (mk-actual te)))
