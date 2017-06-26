cf7e98402807a13244a7182c3f0e51aee165e670
Sam Owre <owre@csl.sri.com>
2017-06-26 01:21:37 -0700

    Added levenshtein-distance function, in case it becomes useful in the future

--- src/test.lisp
+++ src/test.lisp
@@ -323,3 +323,29 @@
 	  (class-hierarchy* (cdr classes) all-classes)
 	  (class-hierarchy* (append (class-direct-subclasses (car classes)) (cdr classes))
 			    (cons (car classes) all-classes)))))
+
+;;; From https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#Common_Lisp
+(defun levenshtein-distance (str1 str2)
+  "Calculates the Levenshtein distance between str1 and str2, returns an editing distance (int)."
+  (let ((n (length str1))
+	(m (length str2)))
+    ;; Check trivial cases
+    (cond ((= 0 n) (return-from levenshtein-distance m))
+	  ((= 0 m) (return-from levenshtein-distance n)))
+    (let ((col (make-array (1+ m) :element-type 'integer))
+	  (prev-col (make-array (1+ m) :element-type 'integer)))
+      ;; We need to store only two columns---the current one that
+      ;; is being built and the previous one
+      (dotimes (i (1+ m))
+	(setf (svref prev-col i) i))
+      ;; Loop across all chars of each string
+      (dotimes (i n)
+	(setf (svref col 0) (1+ i))
+	(dotimes (j m)
+	  (setf (svref col (1+ j))
+		(min (1+ (svref col j))
+		     (1+ (svref prev-col (1+ j)))
+		     (+ (svref prev-col j)
+			(if (char-equal (schar str1 i) (schar str2 j)) 0 1)))))
+	(rotatef col prev-col))
+      (svref prev-col m))))
