df7c4032463b20eb601fa905bc8ad36af0dcb267
Sam Owre <owre@csl.sri.com>
2018-08-15 18:40:04 -0700

    Modified mapping-lhs-theory-context (modname) to not get the theory,
    since it's not needed.

--- src/typecheck.lisp
+++ src/typecheck.lisp
@@ -1272,16 +1272,14 @@
 ;;; given by renamings.
 
 (defmethod mapping-lhs-theory-context ((thname modname))
-  (let ((theory (get-theory thname)))
-    (assert theory)
-    (unless (every #'typed? (actuals thname))
-      (set-type-actuals thname theory))
-    (assert (every #'typed? (actuals thname)))
-    ;;(assert (null (resolutions thname)))
-    (let ((res (resolve* (lcopy thname :mappings nil) 'module nil)))
-      (when res
-	(assert (null (cdr res)))
-	(mapping-lhs-theory-context (declaration (car res)))))))
+  (unless (every #'typed? (actuals thname))
+    (set-type-actuals thname theory))
+  (assert (every #'typed? (actuals thname)))
+  ;;(assert (null (resolutions thname)))
+  (let ((res (resolve* (lcopy thname :mappings nil) 'module nil)))
+    (when res
+      (assert (null (cdr res)))
+      (mapping-lhs-theory-context (declaration (car res))))))
 
 (defmethod mapping-lhs-theory-context ((thdecl theory-abbreviation-decl))
   (assert (resolution (theory-name thdecl)))
