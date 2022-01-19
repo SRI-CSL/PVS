4006b29ccf93af0270da82affea36e7717d60052
Sam Owre <owre@csl.sri.com>
2022-01-18 18:12:06 -0800

    Changed uiop:file-exists-p to uiop:probe-file*

--- src/utils/file-utils-sbcl.lisp
+++ src/utils/file-utils-sbcl.lisp
@@ -23,7 +23,7 @@
 	  file-write-time get-file-info))
 
 (defun file-exists-p (file)
-   (uiop:file-exists-p (sb-ext:parse-native-namestring file)))
+   (uiop:probe-file* (sb-ext:parse-native-namestring file) :truename t))
 
 (defun directory-p (dir)
   (uiop:directory-exists-p dir))
@@ -33,7 +33,7 @@
     (sb-posix:syscall-error () nil)))
 
 (defun write-permission? (&optional (file *default-pathname-defaults*))
-  (let ((pfile (uiop:file-exists-p file)))
+  (let ((pfile (uiop:probe-file* file :truename t)))
     (and pfile
 	 (handler-case (zerop (sb-posix:access pfile sb-posix:w-ok))
 	   (sb-posix:syscall-error () nil)))))
@@ -47,7 +47,7 @@
 	   (sb-posix:syscall-error () nil)))))
 
 (defun get-file-info (file)
-  (let ((pfile (uiop:file-exists-p file)))
+  (let ((pfile (uiop:probe-file* file :truename t)))
     (and pfile
 	 (handler-case
 	     (let ((stat (sb-posix:stat pfile)))
