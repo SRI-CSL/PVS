fa86281fa0b47e3e9c961112c04a3b7ee0302b81
Mariano Moscato <mariano.m.moscato@nasa.gov>
2024-02-27 09:01:35 -0500

    Add case for id-root (its absence caused an error when collecting tccs for datatypes)

--- src/utils.lisp
+++ src/utils.lisp
@@ -1783,6 +1783,8 @@ prove itself from the mapped axioms."
 (defmethod id-root ((decl declaration))
   (op-to-id decl))
 
+(defmethod id-root ((dt datatype))
+  (id dt))
 
 ;;; lambda-depth - returns the number of lambdas in the
 ;;; right-hand-side of the given definition.  E.g.
