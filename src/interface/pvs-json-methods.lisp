e3e0550c9dab4bb693cc100231205284b794b7b4
Mariano Moscato <mariano.m.moscato@nasa.gov>
2026-06-29 13:32:54 -0400

    Add fix for datatype files generation on Mac and defrequest to handle ppe in server mode

--- src/interface/pvs-json-methods.lisp
+++ src/interface/pvs-json-methods.lisp
@@ -674,3 +674,18 @@ Returns JSON of the form:
       `(("main-file" . ,(format nil "~a" main-tex-file))))))
 
 ;; END LaTeX Generation ---------------------------------------------------
+
+;; BEGIN Pretty-print Expanded --------------------------------------------
+
+(defrequest prettyprint-expanded (theory-ref)
+  "Returns the pretty-print expanded form of the given theory"
+  (let ((theory (pvs::get-typechecked-theory theory-ref))
+        (pvs::*no-comments* nil)
+        (pvs::*unparse-expanded* t)
+        (pvs::*xt-periods-allowed* t))
+    (let ((thstring (pvs::unparse theory
+                        :string t
+                        :char-width sb-runtime::*default-char-width*)))
+                    thstring)))
+
+;; END Pretty-print Expanded ----------------------------------------------
